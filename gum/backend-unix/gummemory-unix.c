da598de8dd5cc4f3bc49363bd2f957aa53ab08e7
Ole André Vadla Ravnås <oleavr@gmail.com>
2011-01-10 00:55:10 +0100

    Optimize UNIX backend implementation of gum_alloc_n_pages_near.
    Instead of walking page by page from the near_address we now instead iterate
    over unmapped ranges and consider both ends of each if close enough.

--- gum/backend-unix/gummemory-unix.c
+++ gum/backend-unix/gummemory-unix.c
@@ -34,6 +34,21 @@
 #define GUM_MEMRANGE_IS_NOT_MAPPED(address, size) \
     (gum_memory_get_protection (address, size, NULL) == FALSE)
 
+typedef gboolean (* GumFoundFreeRangeFunc) (const GumMemoryRange * range,
+    gpointer user_data);
+
+typedef struct _GumAllocNearContext GumAllocNearContext;
+
+struct _GumAllocNearContext
+{
+  gpointer result;
+  gsize size;
+  gint unix_page_prot;
+  GumAddressSpec * address_spec;
+};
+
+static gboolean gum_try_alloc_in_range_if_near_enough (
+    const GumMemoryRange * range, gpointer user_data);
 static gint gum_page_protection_to_unix (GumPageProtection page_prot);
 
 void
@@ -50,6 +65,49 @@ guint
 gum_query_page_size (void)
 {
   return sysconf (_SC_PAGE_SIZE);
+}
+
+static void
+gum_memory_enumerate_free_ranges (GumFoundFreeRangeFunc func,
+                                  gpointer user_data)
+{
+  FILE * fp;
+  gboolean carry_on = TRUE;
+  gchar line[1024 + 1];
+  gpointer prev_end = NULL;
+
+  fp = fopen ("/proc/self/maps", "r");
+  g_assert (fp != NULL);
+
+  while (carry_on && fgets (line, sizeof (line), fp) != NULL)
+  {
+    gint n;
+    gpointer start, end;
+
+    n = sscanf (line, "%p-%p ", &start, &end);
+    g_assert_cmpint (n, ==, 2);
+
+    if (prev_end != NULL)
+    {
+      gsize gap_size;
+
+      gap_size = start - prev_end;
+
+      if (gap_size > 0)
+      {
+        GumMemoryRange r;
+
+        r.base_address = prev_end;
+        r.size = gap_size;
+
+        carry_on = func (&r, user_data);
+      }
+    }
+
+    prev_end = end;
+  }
+
+  fclose (fp);
 }
 
 static gboolean
@@ -286,57 +344,58 @@ gum_alloc_n_pages_near (guint n_pages,
                         GumPageProtection page_prot,
                         GumAddressSpec * address_spec)
 {
-  gpointer result = NULL;
-  gsize page_size, size;
-  gint unix_page_prot;
-  const gint flags = MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED;
-  guint8 * low_address, * high_address;
+  GumAllocNearContext ctx;
+  gsize page_size;
 
   page_size = gum_query_page_size ();
-  size = (1 + n_pages) * page_size;
-  unix_page_prot = gum_page_protection_to_unix (page_prot);
 
-  low_address = (guint8 *)
-      (GPOINTER_TO_SIZE (address_spec->near_address) & ~(page_size - 1));
-  high_address = low_address;
+  ctx.result = NULL;
+  ctx.size = (1 + n_pages) * page_size;
+  ctx.unix_page_prot = gum_page_protection_to_unix (page_prot);
+  ctx.address_spec = address_spec;
 
-  do
+  gum_memory_enumerate_free_ranges (gum_try_alloc_in_range_if_near_enough,
+      &ctx);
+
+  g_assert (ctx.result != NULL);
+
+  gum_mprotect (ctx.result, page_size, GUM_PAGE_RW);
+  *((gsize *) ctx.result) = ctx.size;
+  gum_mprotect (ctx.result, page_size, GUM_PAGE_READ);
+
+  return ctx.result + page_size;
+}
+
+static gboolean
+gum_try_alloc_in_range_if_near_enough (const GumMemoryRange * range,
+                                       gpointer user_data)
+{
+  GumAllocNearContext * ctx = user_data;
+  gpointer base_address;
+  gsize distance;
+
+  if (range->size < ctx->size)
+    return TRUE;
+
+  base_address = range->base_address;
+  distance = ABS (ctx->address_spec->near_address - base_address);
+  if (distance > ctx->address_spec->max_distance)
   {
-    gsize cur_distance;
-
-    low_address -= page_size;
-    high_address += page_size;
-    cur_distance = (gsize) high_address + page_size -
-        (gsize) address_spec->near_address;
-    if (cur_distance > address_spec->max_distance)
-      break;
-
-    if (GUM_MEMRANGE_IS_NOT_MAPPED (low_address, size))
-    {
-      result = mmap (low_address, size, unix_page_prot, flags, -1, 0);
-      if (result == MAP_FAILED)
-        result = NULL;
-    }
-
-    if (result == NULL)
-    {
-      if (GUM_MEMRANGE_IS_NOT_MAPPED (high_address, size))
-      {
-        result = mmap (high_address, size, unix_page_prot, flags, -1, 0);
-        if (result == MAP_FAILED)
-          result = NULL;
-      }
-    }
+    base_address = range->base_address + range->size - ctx->size;
+    distance = ABS (ctx->address_spec->near_address - base_address);
   }
-  while (result == NULL);
 
-  g_assert (result != NULL);
+  if (distance > ctx->address_spec->max_distance)
+    return TRUE;
 
-  gum_mprotect (result, page_size, GUM_PAGE_RW);
-  *((gsize *) result) = size;
-  gum_mprotect (result, page_size, GUM_PAGE_READ);
+  ctx->result = mmap (base_address, ctx->size, ctx->unix_page_prot,
+      MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+  if (ctx->result == MAP_FAILED)
+    ctx->result = NULL;
+  else
+    return FALSE;
 
-  return result + page_size;
+  return TRUE;
 }
 
 void
