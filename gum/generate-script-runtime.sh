5a32896fb1858b8ed33adad2fee45b9b47be866f
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-01-25 23:16:59 +0100

    Update runtime build script

--- gum/generate-script-runtime.sh
+++ gum/generate-script-runtime.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-coffee -cb gumscript-runtime.coffee 
+uglifyjs2 -mco gumscript-runtime.js gumscript-runtime-core.js gumscript-runtime-objc.js
 python js2c.py < gumscript-runtime.js > gumscript-runtime.h
 rm gumscript-runtime.js
