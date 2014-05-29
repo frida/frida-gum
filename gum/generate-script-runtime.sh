8f650642544f45d453ca773ed4eddf84e12d6e27
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-05-29 15:28:00 +0200

    Tweak uglifyjs options to disable IE-specific quirks

--- gum/generate-script-runtime.sh
+++ gum/generate-script-runtime.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-uglifyjs -mco gumscript-runtime.js gumscript-runtime-core.js gumscript-runtime-objc.js
+uglifyjs --screw-ie8 -mco gumscript-runtime.js gumscript-runtime-core.js gumscript-runtime-objc.js gumscript-runtime-dalvik.js
 python js2c.py < gumscript-runtime.js > gumscript-runtime.h
 rm gumscript-runtime.js
