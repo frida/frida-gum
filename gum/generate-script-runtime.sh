f5875aa00ca82eb768e69f2a678dfc17306fbec0
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-05-29 14:15:09 +0200

    Update runtime autogen script to use the new uglifyjs CLI tool name

--- gum/generate-script-runtime.sh
+++ gum/generate-script-runtime.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-uglifyjs2 -mco gumscript-runtime.js gumscript-runtime-core.js gumscript-runtime-objc.js
+uglifyjs -mco gumscript-runtime.js gumscript-runtime-core.js gumscript-runtime-objc.js
 python js2c.py < gumscript-runtime.js > gumscript-runtime.h
 rm gumscript-runtime.js
