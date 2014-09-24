d2ef62949dd7e1134483f9dee7dd178d2b4f71ea
Ole André Vadla Ravnås <ole.andre.ravnas@tillitech.com>
2014-09-25 01:03:48 +0200

    Fix license header mixup

--- gum/gumhash.c
+++ gum/gumhash.c
@@ -1,7 +1,18 @@
 /* GLIB - Library of useful routines for C programming
  * Copyright (C) 1995-1997  Peter Mattis, Spencer Kimball and Josh MacDonald
  *
- * Licence: wxWindows Library Licence, Version 3.1
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
