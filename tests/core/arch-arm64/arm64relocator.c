e9cbe9ba8a7a9a464f6812b611fcf5721ff5d871
Ole André Vadla Ravnås <oleavr@gmail.com>
2015-05-31 21:51:15 +0200

    Add tests for relocation of `BR` instruction

--- tests/core/arch-arm64/arm64relocator.c
+++ tests/core/arch-arm64/arm64relocator.c
@@ -14,6 +14,8 @@ TEST_LIST_BEGIN (arm64relocator)
   TESTENTRY (b_cond_should_be_rewritten)
   TESTENTRY (b_should_be_rewritten)
   TESTENTRY (bl_should_be_rewritten)
+  TESTENTRY (cannot_relocate_with_early_br)
+  TESTENTRY (eob_and_eoi_on_br)
   TESTENTRY (eob_and_eoi_on_ret)
 TEST_LIST_END ()
 
@@ -233,6 +235,30 @@ branch_scenario_execute (BranchScenario * bs,
   gum_arm64_writer_flush (&fixture->aw);
   g_assert_cmpint (memcmp (fixture->output, bs->expected_output,
       bs->expected_output_length * sizeof (guint32)), ==, 0);
+}
+
+TESTCASE (cannot_relocate_with_early_br)
+{
+  guint32 input[] = {
+    GUINT32_TO_LE (0x58000050), /* ldr x16, [pc, #8] */
+    GUINT32_TO_LE (0xd61f0200)  /* br x16            */
+  };
+
+  g_assert (!gum_arm64_relocator_can_relocate (input, 16));
+}
+
+TESTCASE (eob_and_eoi_on_br)
+{
+  const guint32 input[] = {
+    GUINT32_TO_LE (0xd61f0200)  /* br x16 */
+  };
+
+  SETUP_RELOCATOR_WITH (input);
+
+  g_assert_cmpuint (gum_arm64_relocator_read_one (&fixture->rl, NULL), ==, 4);
+  g_assert (gum_arm64_relocator_eob (&fixture->rl));
+  g_assert (gum_arm64_relocator_eoi (&fixture->rl));
+  g_assert_cmpuint (gum_arm64_relocator_read_one (&fixture->rl, NULL), ==, 0);
 }
 
 TESTCASE (eob_and_eoi_on_ret)
