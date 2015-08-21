fa075b5cccfbd874221780322f6f24a3e5a4a69a
Ole André Vadla Ravnås <oleavr@nowsecure.com>
2015-08-21 15:32:22 +0200

    Speed up the fallback enumeration of modules
    We only need to examine ranges that have an associated file mapping.

--- gum/backend-darwin/gumprocess-darwin.c
+++ gum/backend-darwin/gumprocess-darwin.c
@@ -1064,7 +1064,8 @@ gum_store_range_of_potential_modules (const GumRangeDetails * details,
 {
   GumEnumerateModulesSlowContext * ctx = user_data;
 
-  g_array_append_val (ctx->ranges, *(details->range));
+  if (details->file != NULL)
+    g_array_append_val (ctx->ranges, *(details->range));
 
   return TRUE;
 }
