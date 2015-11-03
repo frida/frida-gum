9637d77038e0951bd17f5acec1a90212a333cdad
Ole André Vadla Ravnås <oleavr@nowsecure.com>
2015-11-03 18:14:41 +0100

    Fix the Babel build step

--- bindings/gumjs/generate-runtime.py
+++ bindings/gumjs/generate-runtime.py
@@ -51,7 +51,7 @@ static const {entry_type} {entries_identifier}[] =
             input_name_es5 = base + "-es5" + ext
             input_path_es5 = os.path.join(output_dir, input_name_es5)
 
-            subprocess.call([os.path.join(output_dir, "/node_modules/babel-cli/bin/babel.js"), "--presets", "es2015", input_path_es6, "-o", input_path_es5], cwd=output_dir)
+            subprocess.call([os.path.join(output_dir, "node_modules/.bin/babel"), "--presets", "es2015", input_path_es6, "-o", input_path_es5], cwd=output_dir)
 
             output_file.write("""
   {{
