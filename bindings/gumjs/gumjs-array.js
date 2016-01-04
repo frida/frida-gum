06b06c1634d0a18514110304b9bb12c24c03ae64
s1341 <github@shmarya.net>
2016-01-05 00:43:49 +0100

    Add polyfills for Array.from and Array.prototype.includes

---
+++ bindings/gumjs/gumjs-array.js
@@ -0,0 +1,39 @@
+/**
+* @license MIT, GPL, do whatever you want
+* @requires polyfill: Array.prototype.slice fix {@link https://gist.github.com/brettz9/6093105}
+*/
+if (!Array.from) {
+    Array.from = function (object) {
+        'use strict';
+        return [].slice.call(object);
+    };
+}
+
+if (![].includes) {
+  Array.prototype.includes = function(searchElement /*, fromIndex*/ ) {
+    'use strict';
+    var O = Object(this);
+    var len = parseInt(O.length) || 0;
+    if (len === 0) {
+      return false;
+    }
+    var n = parseInt(arguments[1]) || 0;
+    var k;
+    if (n >= 0) {
+      k = n;
+    } else {
+      k = len + n;
+      if (k < 0) {k = 0;}
+    }
+    var currentElement;
+    while (k < len) {
+      currentElement = O[k];
+      if (searchElement === currentElement ||
+         (searchElement !== searchElement && currentElement !== currentElement)) {
+        return true;
+      }
+      k++;
+    }
+    return false;
+  };
+}
