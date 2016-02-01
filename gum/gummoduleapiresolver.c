1d9eaa4994719f376cde85a9741a59ac5c7b8ce5
Ole André Vadla Ravnås <oleavr@nowsecure.com>
2016-02-01 19:30:43 +0100

    Use the fully-qualified module name

--- gum/gummoduleapiresolver.c
+++ gum/gummoduleapiresolver.c
@@ -164,7 +164,7 @@ gum_module_api_resolver_enumerate_matches (GumApiResolver * resolver,
         {
           GumApiDetails details;
 
-          details.name = g_strconcat (module->name, "!", function->name, NULL);
+          details.name = g_strconcat (module->path, "!", function->name, NULL);
           details.address = function->address;
 
           carry_on = func (&details, user_data);
