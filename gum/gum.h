30ecc30fd14eea33c62eafa29aad37c7a6e9ca77
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-07-28 17:03:57 +0200

    Add missing includes to the umbrella header

--- gum/gum.h
+++ gum/gum.h
@@ -9,20 +9,29 @@
 
 #include <gum/gumdefs.h>
 
+#include <gum/gumapiresolver.h>
 #include <gum/gumbacktracer.h>
+#include <gum/gumcodeallocator.h>
+#include <gum/gumcodesegment.h>
+#include <gum/gumexceptor.h>
 #include <gum/gumevent.h>
 #include <gum/gumeventsink.h>
+#include <gum/gumfunction.h>
 #include <gum/guminterceptor.h>
+#include <gum/guminvocationcontext.h>
 #include <gum/guminvocationlistener.h>
 #include <gum/gumkernel.h>
 #include <gum/gummemory.h>
 #include <gum/gummemoryaccessmonitor.h>
 #include <gum/gummemorymap.h>
+#include <gum/gummoduleapiresolver.h>
 #include <gum/gummodulemap.h>
 #include <gum/gumprocess.h>
 #include <gum/gumreturnaddress.h>
+#include <gum/gumspinlock.h>
 #include <gum/gumstalker.h>
 #include <gum/gumsymbolutil.h>
+#include <gum/gumsysinternals.h>
 #include <gum/gumtls.h>
 
 G_BEGIN_DECLS
