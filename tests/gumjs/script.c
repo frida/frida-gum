3627f91c9c285bca5b18e4ce91918583c5e466d4
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-10-11 03:59:49 +0200

    Re-enable disabled Script tests

--- tests/gumjs/script.c
+++ tests/gumjs/script.c
@@ -8,7 +8,6 @@
 #include "script-fixture.c"
 
 TEST_LIST_BEGIN (script)
-#if 0
   SCRIPT_TESTENTRY (invalid_script_should_return_null)
   SCRIPT_TESTENTRY (array_buffer_can_be_created)
   SCRIPT_TESTENTRY (message_can_be_sent)
@@ -153,20 +152,16 @@ TEST_LIST_BEGIN (script)
   SCRIPT_TESTENTRY (module_base_address_can_be_found)
   SCRIPT_TESTENTRY (module_export_can_be_found_by_name)
   SCRIPT_TESTENTRY (api_resolver_can_be_used_to_find_functions)
-#endif
   SCRIPT_TESTENTRY (socket_connection_can_be_established)
-#if 0
   SCRIPT_TESTENTRY (socket_type_can_be_inspected)
 #if !defined (HAVE_ANDROID) && !(defined (HAVE_LINUX) && defined (HAVE_ARM)) && \
   !(defined (HAVE_LINUX) && defined (HAVE_MIPS))
   SCRIPT_TESTENTRY (socket_endpoints_can_be_inspected)
 #endif
-#endif
 #ifdef G_OS_UNIX
   SCRIPT_TESTENTRY (unix_fd_can_be_read_from)
   SCRIPT_TESTENTRY (unix_fd_can_be_written_to)
 #endif
-#if 0
   SCRIPT_TESTENTRY (basic_hexdump_functionality_is_available)
   SCRIPT_TESTENTRY (hexdump_supports_native_pointer_conforming_object)
   SCRIPT_TESTENTRY (native_pointer_provides_is_null)
@@ -200,7 +195,6 @@ TEST_LIST_BEGIN (script)
   SCRIPT_TESTENTRY (globals_can_be_dynamically_generated)
   SCRIPT_TESTENTRY (exceptions_can_be_handled)
   SCRIPT_TESTENTRY (debugger_can_be_enabled)
-#endif
 TEST_LIST_END ()
 
 typedef struct _TestTrigger TestTrigger;
