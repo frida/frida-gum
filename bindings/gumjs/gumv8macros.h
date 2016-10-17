d4de5dcf5d5eecffb78faa16df40b20c91c9cf4a
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-10-17 14:07:20 +0200

    Add macros for class constructors and methods

--- bindings/gumjs/gumv8macros.h
+++ bindings/gumjs/gumv8macros.h
@@ -11,17 +11,19 @@
 
 #define GUMJS_MODULE_TYPE G_PASTE (GumV8, GUMJS_MODULE_NAME)
 
+#define GUMJS_DECLARE_CONSTRUCTOR GUMJS_DECLARE_FUNCTION
 #define GUMJS_DECLARE_FUNCTION(N) \
   static void N (const FunctionCallbackInfo<Value> & info);
 
+#define GUMJS_DEFINE_CONSTRUCTOR GUMJS_DEFINE_FUNCTION
 #define GUMJS_DEFINE_FUNCTION(N) \
   static void N##_impl (GUMJS_MODULE_TYPE * module, const GumV8Args * args); \
   \
   static void \
   N (const FunctionCallbackInfo<Value> & info) \
   { \
-    GUMJS_MODULE_TYPE * module = (GUMJS_MODULE_TYPE *) \
-        info.Data ().As<External> ()->Value (); \
+    auto module = static_cast<GUMJS_MODULE_TYPE *> ( \
+        info.Data ().As<External> ()->Value ()); \
     \
     GumV8Args args; \
     args.info = &info; \
@@ -32,6 +34,25 @@
   \
   static void \
   N##_impl (GUMJS_MODULE_TYPE * module, \
+            const GumV8Args * args)
+#define GUMJS_DEFINE_METHOD(C, N) \
+  static void N##_impl (G_PASTE (GumV8, C) * self, const GumV8Args * args); \
+  \
+  static void \
+  N (const FunctionCallbackInfo<Value> & info) \
+  { \
+    auto self = static_cast<G_PASTE (GumV8, C) *> ( \
+        info.Holder ()->GetAlignedPointerFromInternalField (0)); \
+    \
+    GumV8Args args; \
+    args.info = &info; \
+    args.core = self->module->core; \
+    \
+    return N##_impl (self, &args); \
+  } \
+  \
+  static void \
+  N##_impl (G_PASTE (GumV8, C) * self, \
             const GumV8Args * args)
 
 #endif
