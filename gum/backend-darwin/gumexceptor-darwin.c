e44545bdc6e688de4f3649861291250096581906
Ole André Vadla Ravnås <oleavr@gmail.com>
2016-11-09 17:51:40 +0100

    Implement handling of SIGABRT

--- gum/backend-darwin/gumexceptor-darwin.c
+++ gum/backend-darwin/gumexceptor-darwin.c
@@ -57,7 +57,9 @@ struct _GumExceptorBackend
   gpointer handler_data;
 
   mach_port_name_t server_port;
-  GumExceptionPortSet previous_ports;
+  GumExceptionPortSet old_ports;
+  gboolean old_abort_handler_present;
+  struct sigaction old_abort_handler;
   GThread * worker;
 
   GumInterceptor * interceptor;
@@ -70,6 +72,13 @@ static void gum_exceptor_backend_detach (GumExceptorBackend * self);
 static void gum_exceptor_backend_send_stop_request (GumExceptorBackend * self);
 static gpointer gum_exceptor_backend_process_messages (
     GumExceptorBackend * self);
+static sig_t gum_exceptor_backend_replacement_signal (int sig, sig_t handler);
+static int gum_exceptor_backend_replacement_sigaction (int sig,
+    const struct sigaction * act, struct sigaction * oact);
+static void gum_exceptor_backend_on_signal (int sig, siginfo_t * siginfo,
+    void * context);
+
+static gboolean gum_is_signal_handler_chainable (sig_t handler);
 
 static gboolean gum_exception_memory_details_from_thread (
     mach_port_t thread, GumExceptionMemoryDetails * md);
@@ -132,9 +141,11 @@ gum_exceptor_backend_new (GumExceptionHandler handler,
 static void
 gum_exceptor_backend_attach (GumExceptorBackend * self)
 {
+  GumInterceptor * interceptor = self->interceptor;
   mach_port_name_t self_task;
   kern_return_t kr;
-  GumExceptionPortSet * previous_ports;
+  GumExceptionPortSet * old_ports;
+  struct sigaction action;
 
   self_task = mach_task_self ();
 
@@ -146,7 +157,7 @@ gum_exceptor_backend_attach (GumExceptorBackend * self)
       MACH_MSG_TYPE_MAKE_SEND);
   g_assert_cmpint (kr, ==, KERN_SUCCESS);
 
-  previous_ports = &self->previous_ports;
+  old_ports = &self->old_ports;
   kr = task_swap_exception_ports (self_task,
       EXC_MASK_ARITHMETIC |
       EXC_MASK_BAD_ACCESS |
@@ -157,14 +168,27 @@ gum_exceptor_backend_attach (GumExceptorBackend * self)
       self->server_port,
       EXCEPTION_STATE_IDENTITY | MACH_EXCEPTION_CODES,
       GUM_DARWIN_THREAD_STATE_FLAVOR,
-      previous_ports->masks,
-      &previous_ports->count,
-      previous_ports->ports,
-      previous_ports->behaviors,
-      previous_ports->flavors);
+      old_ports->masks,
+      &old_ports->count,
+      old_ports->ports,
+      old_ports->behaviors,
+      old_ports->flavors);
   g_assert_cmpint (kr, ==, KERN_SUCCESS);
 
-  /* TODO: SIGABRT */
+  self->old_abort_handler_present = TRUE;
+  action.sa_sigaction = gum_exceptor_backend_on_signal;
+  sigemptyset (&action.sa_mask);
+  action.sa_flags = SA_SIGINFO;
+  sigaction (SIGABRT, &action, &self->old_abort_handler);
+
+  gum_interceptor_begin_transaction (interceptor);
+
+  gum_interceptor_replace_function (interceptor, signal,
+      gum_exceptor_backend_replacement_signal, self);
+  gum_interceptor_replace_function (interceptor, sigaction,
+      gum_exceptor_backend_replacement_sigaction, self);
+
+  gum_interceptor_end_transaction (interceptor);
 
   self->worker = g_thread_new ("gum-exceptor-worker",
       (GThreadFunc) gum_exceptor_backend_process_messages, self);
@@ -173,25 +197,33 @@ gum_exceptor_backend_attach (GumExceptorBackend * self)
 static void
 gum_exceptor_backend_detach (GumExceptorBackend * self)
 {
+  GumInterceptor * interceptor = self->interceptor;
   mach_port_name_t self_task;
-  GumExceptionPortSet * previous_ports;
+  GumExceptionPortSet * old_ports;
   mach_msg_type_number_t port_index;
+
+  gum_interceptor_begin_transaction (interceptor);
+
+  gum_interceptor_revert_function (interceptor, signal);
+  gum_interceptor_revert_function (interceptor, sigaction);
+
+  gum_interceptor_end_transaction (interceptor);
 
   self_task = mach_task_self ();
 
-  previous_ports = &self->previous_ports;
-  for (port_index = 0; port_index != previous_ports->count; port_index++)
+  old_ports = &self->old_ports;
+  for (port_index = 0; port_index != old_ports->count; port_index++)
   {
     kern_return_t kr;
 
     kr = task_set_exception_ports (self_task,
-        previous_ports->masks[port_index],
-        previous_ports->ports[port_index],
-        previous_ports->behaviors[port_index],
-        previous_ports->flavors[port_index]);
+        old_ports->masks[port_index],
+        old_ports->ports[port_index],
+        old_ports->behaviors[port_index],
+        old_ports->flavors[port_index]);
     g_assert_cmpint (kr, ==, KERN_SUCCESS);
   }
-  previous_ports->count = 0;
+  old_ports->count = 0;
 
   gum_exceptor_backend_send_stop_request (self);
   g_thread_join (self->worker);
@@ -364,7 +396,7 @@ catch_mach_exception_raise_state_identity (
   }
   else
   {
-    GumExceptionPortSet * previous_ports = &self->previous_ports;
+    GumExceptionPortSet * old_ports = &self->old_ports;
     mach_msg_type_number_t port_index;
     exception_data_t small_code;
     mach_msg_type_number_t code_index;
@@ -377,11 +409,11 @@ catch_mach_exception_raise_state_identity (
 
     kr = KERN_FAILURE;
 
-    for (port_index = 0; port_index != previous_ports->count; port_index++)
+    for (port_index = 0; port_index != old_ports->count; port_index++)
     {
-      exception_mask_t mask = previous_ports->masks[port_index];
-      mach_port_t port = previous_ports->ports[port_index];
-      exception_behavior_t behavior = previous_ports->behaviors[port_index];
+      exception_mask_t mask = old_ports->masks[port_index];
+      mach_port_t port = old_ports->ports[port_index];
+      exception_behavior_t behavior = old_ports->behaviors[port_index];
       gboolean is_modern;
 
       if (port == MACH_PORT_NULL)
@@ -464,6 +496,138 @@ catch_mach_exception_raise_state_identity (
   mach_port_deallocate (self_task, task);
 
   return kr;
+}
+
+static sig_t
+gum_exceptor_backend_replacement_signal (int sig,
+                                         sig_t handler)
+{
+  GumExceptorBackend * self;
+  GumInvocationContext * ctx;
+  struct sigaction * old_handler;
+  sig_t result;
+
+  ctx = gum_interceptor_get_current_invocation ();
+  g_assert (ctx != NULL);
+
+  self = GUM_EXCEPTOR_BACKEND (
+      gum_invocation_context_get_replacement_function_data (ctx));
+
+  if (sig != SIGABRT || !self->old_abort_handler_present)
+    return signal (sig, handler);
+
+  old_handler = &self->old_abort_handler;
+
+  result = ((old_handler->sa_flags & SA_SIGINFO) == 0)
+      ? old_handler->sa_handler
+      : SIG_DFL;
+
+  old_handler->sa_handler = handler;
+  old_handler->sa_flags &= ~SA_SIGINFO;
+
+  return result;
+}
+
+static int
+gum_exceptor_backend_replacement_sigaction (int sig,
+                                            const struct sigaction * act,
+                                            struct sigaction * oact)
+{
+  GumExceptorBackend * self;
+  GumInvocationContext * ctx;
+  struct sigaction * old_handler;
+
+  ctx = gum_interceptor_get_current_invocation ();
+  g_assert (ctx != NULL);
+
+  self = GUM_EXCEPTOR_BACKEND (
+      gum_invocation_context_get_replacement_function_data (ctx));
+
+  if (sig != SIGABRT || !self->old_abort_handler_present)
+    return sigaction (sig, act, oact);
+
+  old_handler = &self->old_abort_handler;
+
+  if (oact != NULL)
+    *oact = *old_handler;
+  if (act != NULL)
+    *old_handler = *act;
+
+  return 0;
+}
+
+static void
+gum_exceptor_backend_on_signal (int sig,
+                                siginfo_t * siginfo,
+                                void * context)
+{
+  GumExceptorBackend * self = the_backend;
+  ucontext_t * uc = context;
+  GumExceptionDetails ed;
+  GumExceptionMemoryDetails * md = &ed.memory;
+  GumCpuContext * cpu_context = &ed.context;
+  struct sigaction * action;
+
+  g_assert_cmpint (sig, ==, SIGABRT);
+
+  action = &self->old_abort_handler;
+
+  ed.thread_id = gum_process_get_current_thread_id ();
+  ed.type = GUM_EXCEPTION_ABORT;
+
+  gum_darwin_parse_native_thread_state (&uc->uc_mcontext->__ss, cpu_context);
+  ed.native_context = context;
+
+#if defined (HAVE_I386)
+  ed.address = GSIZE_TO_POINTER (GUM_CPU_CONTEXT_XIP (cpu_context));
+#elif defined (HAVE_ARM) || defined (HAVE_ARM64)
+  ed.address = GSIZE_TO_POINTER (cpu_context->pc);
+#elif defined (HAVE_MIPS)
+  ed.address = GSIZE_TO_POINTER (cpu_context->pc);
+#else
+# error Unsupported architecture
+#endif
+
+  md->operation = GUM_MEMOP_INVALID;
+  md->address = NULL;
+
+  if (self->handler (&ed, self->handler_data))
+  {
+    gum_darwin_unparse_native_thread_state (cpu_context,
+        &uc->uc_mcontext->__ss);
+    return;
+  }
+
+  if ((action->sa_flags & SA_SIGINFO) != 0)
+  {
+    void (* old_sigaction) (int, siginfo_t *, void *) = action->sa_sigaction;
+
+    if (old_sigaction != NULL)
+      old_sigaction (sig, siginfo, context);
+    else
+      goto panic;
+  }
+  else
+  {
+    void (* old_handler) (int) = action->sa_handler;
+
+    if (gum_is_signal_handler_chainable (old_handler))
+      old_handler (sig);
+    else if (action->sa_handler != SIG_IGN)
+      goto panic;
+  }
+
+  return;
+
+panic:
+  self->old_abort_handler_present = FALSE;
+  signal (SIGABRT, SIG_DFL);
+}
+
+static gboolean
+gum_is_signal_handler_chainable (sig_t handler)
+{
+  return handler != SIG_DFL && handler != SIG_IGN && handler != SIG_ERR;
 }
 
 static gboolean
