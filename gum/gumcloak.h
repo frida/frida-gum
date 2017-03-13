9eb7433bfeac3f654936b539199d5a68b3056d88
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-03-13 13:54:12 +0100

    Add thread cloaking API

--- gum/gumcloak.h
+++ gum/gumcloak.h
@@ -8,12 +8,17 @@
 #define __GUM_CLOAK_H__
 
 #include <gum/gummemory.h>
+#include <gum/gumprocess.h>
 
 G_BEGIN_DECLS
 
 GUM_API void gum_cloak_add_range (const GumMemoryRange * range);
 GUM_API void gum_cloak_remove_range (const GumMemoryRange * range);
 GUM_API GArray * gum_cloak_clip_range (const GumMemoryRange * range);
+
+GUM_API void gum_cloak_add_thread (GumThreadId id);
+GUM_API void gum_cloak_remove_thread (GumThreadId id);
+GUM_API gboolean gum_cloak_has_thread (GumThreadId id);
 
 G_END_DECLS
 
