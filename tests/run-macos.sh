bdeac07a65b9ddb683c477db2599b7246ba0ab87
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-07-14 00:31:39 +0200

    Add a helper script for running tests on macOS

---
+++ tests/run-macos.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+arch=x86_64
+
+gum_tests=$(dirname "$0")
+cd "$gum_tests/../../build/tmp-macos-$arch/frida-gum" || exit 1
+. ../../frida-meson-env-macos-x86_64.rc
+ninja || exit 1
+tests/gum-tests "$@"
