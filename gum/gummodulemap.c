538ef92b2967084ecb364505aa1007fd74632388
Ole André Vadla Ravnås <oleavr@gmail.com>
2017-09-07 14:11:45 +0200

    Optimize module lookup by doing binary search

--- gum/gummodulemap.c
+++ gum/gummodulemap.c
@@ -6,6 +6,8 @@
 
 #include "gummodulemap.h"
 
+#include <stdlib.h>
+
 struct _GumModuleMap
 {
   GObject parent;
@@ -23,6 +25,11 @@ static void gum_module_map_finalize (GObject * object);
 static void gum_module_map_clear (GumModuleMap * self);
 static gboolean gum_add_module (const GumModuleDetails * details,
     gpointer user_data);
+
+static gint gum_module_details_compare_base (const GumModuleDetails * a,
+    const GumModuleDetails * b);
+static gint gum_module_details_compare_to_key (const GumAddress * key_ptr,
+    const GumModuleDetails * member);
 
 G_DEFINE_TYPE (GumModuleMap, gum_module_map, G_TYPE_OBJECT);
 
@@ -100,16 +107,9 @@ const GumModuleDetails *
 gum_module_map_find (GumModuleMap * self,
                      GumAddress address)
 {
-  guint i;
-
-  for (i = 0; i < self->modules->len; i++)
-  {
-    GumModuleDetails * d = &g_array_index (self->modules, GumModuleDetails, i);
-    if (GUM_MEMORY_RANGE_INCLUDES (d->range, address))
-      return d;
-  }
-
-  return NULL;
+  return bsearch (&address, self->modules->data, self->modules->len,
+      sizeof (GumModuleDetails),
+      (GCompareFunc) gum_module_details_compare_to_key);
 }
 
 void
@@ -117,6 +117,7 @@ gum_module_map_update (GumModuleMap * self)
 {
   gum_module_map_clear (self);
   gum_process_enumerate_modules (gum_add_module, self);
+  g_array_sort (self->modules, (GCompareFunc) gum_module_details_compare_base);
 }
 
 static void
@@ -154,4 +155,27 @@ gum_add_module (const GumModuleDetails * details,
   g_array_append_val (self->modules, copy);
 
   return TRUE;
+}
+
+static gint
+gum_module_details_compare_base (const GumModuleDetails * a,
+                                 const GumModuleDetails * b)
+{
+  return (gssize) a->range->base_address - (gssize) b->range->base_address;
+}
+
+static gint
+gum_module_details_compare_to_key (const GumAddress * key_ptr,
+                                   const GumModuleDetails * member)
+{
+  GumAddress key = *key_ptr;
+  const GumMemoryRange * m = member->range;
+
+  if (key < m->base_address)
+    return -1;
+
+  if (key >= m->base_address + m->size)
+    return 1;
+
+  return 0;
 }
