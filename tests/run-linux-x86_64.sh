7a7e877ee34459d6c8a1a9f25a4f646fc4040fe7
Ole André Vadla Ravnås <oleavr@gmail.com>
2018-01-31 01:29:38 +0100

    Add test-runner script for linux-x86_64

---
+++ tests/run-linux-x86_64.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+arch=x86_64
+
+gum_tests=$(dirname "$0")
+cd "$gum_tests/../../build/tmp_thin-linux-$arch/frida-gum" || exit 1
+. ../../frida_thin-meson-env-linux-x86_64.rc
+ninja || exit 1
+tests/gum-tests "$@"
