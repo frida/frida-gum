07d65de3d67b34de558350acb9a19d191abe7dc4
mrmacete <mrmacete@protonmail.ch>
2018-04-24 10:21:19 +0200

    Fix code style

--- gum/backend-darwin/gumexceptor-darwin.c
+++ gum/backend-darwin/gumexceptor-darwin.c
@@ -312,7 +312,9 @@ gum_exceptor_backend_detach (GumExceptorBackend * self)
         old_ports->flavors[port_index]);
     g_assert_cmpint (kr, ==, KERN_SUCCESS);
   }
+
   gum_exceptor_backend_stop_worker_thread (self);
+
   gum_exception_port_set_mod_refs (old_ports, -1);
   old_ports->count = 0;
 
