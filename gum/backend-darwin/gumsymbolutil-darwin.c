371ff0891e95d62f66537b8d088e625aba64d377
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-01-31 00:58:08 +0100

    Fix symbol lookup cache invalidation on i/macOS
    By throwing away our cached symbolicator when we detect that dyld loaded
    or unloaded a library. We hook an internal function in dyld instead of
    _dyld_debugger_notification(), which is the public API that modern LLDB
    versions put a breakpoint on. (In turn resulting in Interceptor refusing
    to hook it.)

--- gum/backend-darwin/gumsymbolutil-darwin.c
+++ gum/backend-darwin/gumsymbolutil-darwin.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2018 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2010-2019 Ole André Vadla Ravnås <oleavr@nowsecure.com>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
@@ -9,79 +9,129 @@
 #include "gum-init.h"
 #include "gumdarwinsymbolicator.h"
 
-static gpointer do_init (gpointer data);
+#include <mach-o/dyld.h>
+
+#define GUM_TYPE_SYMBOL_CACHE_INVALIDATOR \
+    (gum_symbol_cache_invalidator_get_type ())
+G_DECLARE_FINAL_TYPE (GumSymbolCacheInvalidator, gum_symbol_cache_invalidator,
+    GUM, SYMBOL_CACHE_INVALIDATOR, GObject)
+
+struct _GumSymbolCacheInvalidator
+{
+  GObject parent;
+};
+
 static void do_deinit (void);
 
 static GArray * gum_pointer_array_new_empty (void);
 static GArray * gum_pointer_array_new_take_addresses (GumAddress * addresses,
     gsize len);
 
+static void gum_symbol_cache_invalidator_iface_init (gpointer g_iface,
+    gpointer iface_data);
+static void gum_symbol_cache_invalidator_on_dyld_debugger_notification (
+    GumInvocationListener * self, GumInvocationContext * context);
+
+G_LOCK_DEFINE_STATIC (symbolicator);
+static GumDarwinSymbolicator * symbolicator = NULL;
+static GumSymbolCacheInvalidator * invalidator = NULL;
+
+G_DEFINE_TYPE_EXTENDED (GumSymbolCacheInvalidator,
+                        gum_symbol_cache_invalidator,
+                        G_TYPE_OBJECT,
+                        0,
+                        G_IMPLEMENT_INTERFACE (GUM_TYPE_INVOCATION_LISTENER,
+                            gum_symbol_cache_invalidator_iface_init))
+
 static GumDarwinSymbolicator *
-gum_try_get_symbolicator (void)
+gum_try_obtain_symbolicator (void)
 {
-  static GOnce init_once = G_ONCE_INIT;
+  GumDarwinSymbolicator * result = NULL;
 
-  g_once (&init_once, do_init, NULL);
+  G_LOCK (symbolicator);
 
-  return init_once.retval;
-}
+  if (invalidator == NULL)
+  {
+    invalidator = g_object_new (GUM_TYPE_SYMBOL_CACHE_INVALIDATOR, NULL);
 
-static gpointer
-do_init (gpointer data)
-{
-  GumDarwinSymbolicator * symbolicator;
+    _gum_register_early_destructor (do_deinit);
+  }
 
-  symbolicator =
-      gum_darwin_symbolicator_new_with_task (mach_task_self (), NULL);
   if (symbolicator == NULL)
-    return NULL;
+  {
+    symbolicator =
+        gum_darwin_symbolicator_new_with_task (mach_task_self (), NULL);
+  }
 
-  _gum_register_early_destructor (do_deinit);
+  if (symbolicator != NULL)
+    result = g_object_ref (symbolicator);
 
-  return symbolicator;
+  G_UNLOCK (symbolicator);
+
+  return result;
 }
 
 static void
 do_deinit (void)
 {
-  g_object_unref (gum_try_get_symbolicator ());
+  G_LOCK (symbolicator);
+
+  g_clear_object (&symbolicator);
+  g_clear_object (&invalidator);
+
+  G_UNLOCK (symbolicator);
 }
 
 gboolean
 gum_symbol_details_from_address (gpointer address,
                                  GumDebugSymbolDetails * details)
 {
+  gboolean success;
   GumDarwinSymbolicator * symbolicator;
 
-  if ((symbolicator = gum_try_get_symbolicator ()) == NULL)
+  if ((symbolicator = gum_try_obtain_symbolicator ()) == NULL)
     return FALSE;
 
-  return gum_darwin_symbolicator_details_from_address (symbolicator,
+  success = gum_darwin_symbolicator_details_from_address (symbolicator,
       GUM_ADDRESS (address), details);
+
+  g_object_unref (symbolicator);
+
+  return success;
 }
 
 gchar *
 gum_symbol_name_from_address (gpointer address)
 {
+  gchar * name;
   GumDarwinSymbolicator * symbolicator;
 
-  if ((symbolicator = gum_try_get_symbolicator ()) == NULL)
+  if ((symbolicator = gum_try_obtain_symbolicator ()) == NULL)
     return NULL;
 
-  return gum_darwin_symbolicator_name_from_address (symbolicator,
+  name = gum_darwin_symbolicator_name_from_address (symbolicator,
       GUM_ADDRESS (address));
+
+  g_object_unref (symbolicator);
+
+  return name;
 }
 
 gpointer
 gum_find_function (const gchar * name)
 {
+  gpointer address;
   GumDarwinSymbolicator * symbolicator;
 
-  if ((symbolicator = gum_try_get_symbolicator ()) == NULL)
+  if ((symbolicator = gum_try_obtain_symbolicator ()) == NULL)
     return NULL;
 
-  return GSIZE_TO_POINTER (
+  address = GSIZE_TO_POINTER (
       gum_darwin_symbolicator_find_function (symbolicator, name));
+
+  g_object_unref (symbolicator);
+
+  return address;
 }
 
 GArray *
@@ -91,11 +141,13 @@ gum_find_functions_named (const gchar * name)
   GumAddress * addresses;
   gsize len;
 
-  if ((symbolicator = gum_try_get_symbolicator ()) == NULL)
+  if ((symbolicator = gum_try_obtain_symbolicator ()) == NULL)
     return gum_pointer_array_new_empty ();
 
   addresses =
       gum_darwin_symbolicator_find_functions_named (symbolicator, name, &len);
+
+  g_object_unref (symbolicator);
 
   return gum_pointer_array_new_take_addresses (addresses, len);
 }
@@ -107,11 +159,13 @@ gum_find_functions_matching (const gchar * str)
   GumAddress * addresses;
   gsize len;
 
-  if ((symbolicator = gum_try_get_symbolicator ()) == NULL)
+  if ((symbolicator = gum_try_obtain_symbolicator ()) == NULL)
     return gum_pointer_array_new_empty ();
 
   addresses =
       gum_darwin_symbolicator_find_functions_matching (symbolicator, str, &len);
+
+  g_object_unref (symbolicator);
 
   return gum_pointer_array_new_take_addresses (addresses, len);
 }
@@ -140,4 +194,51 @@ gum_pointer_array_new_take_addresses (GumAddress * addresses,
   g_free (addresses);
 
   return result;
+}
+
+static void
+gum_symbol_cache_invalidator_class_init (GumSymbolCacheInvalidatorClass * klass)
+{
+  (void) GUM_IS_SYMBOL_CACHE_INVALIDATOR;
+  (void) GUM_SYMBOL_CACHE_INVALIDATOR;
+  (void) glib_autoptr_cleanup_GumSymbolCacheInvalidator;
+}
+
+static void
+gum_symbol_cache_invalidator_iface_init (gpointer g_iface,
+                                         gpointer iface_data)
+{
+  GumInvocationListenerInterface * iface = g_iface;
+
+  iface->on_enter = gum_symbol_cache_invalidator_on_dyld_debugger_notification;
+}
+
+static void
+gum_symbol_cache_invalidator_init (GumSymbolCacheInvalidator * self)
+{
+  GumDarwinAllImageInfos infos;
+  GumInterceptor * interceptor;
+
+  if (!gum_darwin_query_all_image_infos (mach_task_self (), &infos))
+    return;
+
+  interceptor = gum_interceptor_obtain ();
+
+  gum_interceptor_attach_listener (interceptor,
+      GSIZE_TO_POINTER (infos.notification_address),
+      GUM_INVOCATION_LISTENER (self), NULL);
+
+  g_object_unref (interceptor);
+}
+
+static void
+gum_symbol_cache_invalidator_on_dyld_debugger_notification (
+    GumInvocationListener * self,
+    GumInvocationContext * context)
+{
+  G_LOCK (symbolicator);
+
+  g_clear_object (&symbolicator);
+
+  G_UNLOCK (symbolicator);
 }
