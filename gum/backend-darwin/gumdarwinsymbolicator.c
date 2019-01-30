0ff8264730e8f4c76ee0dbf8680c4216da40a82b
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-01-30 15:24:05 +0100

    Revert symbol cache invalidation fix for i/macOS
    Seems a bit too fragile, e.g. when a debugger is attached, without a
    clear way to know that notifications won't happen.
    
    This reverts commit 5ce54ad29df27c353e5290746ebf3bf5c64c877c.

--- gum/backend-darwin/gumdarwinsymbolicator.c
+++ gum/backend-darwin/gumdarwinsymbolicator.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2019 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2018 Ole André Vadla Ravnås <oleavr@nowsecure.com>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
@@ -24,11 +24,6 @@ typedef CSTypeRef CSSymbolRef;
 typedef CSTypeRef CSSymbolOwnerRef;
 typedef CSTypeRef CSSourceInfoRef;
 
-typedef long CSSymbolicatorFlags;
-typedef gpointer CSNotificationData;
-
-typedef void (^ CSNotificationBlock) (uint32_t notification_type,
-    CSNotificationData data);
 typedef int (^ CSEachSymbolBlock) (CSSymbolRef symbol);
 
 struct _CSTypeRef
@@ -55,11 +50,6 @@ enum
   PROP_PATH,
   PROP_CPU_TYPE,
   PROP_TASK,
-};
-
-enum _CSSymbolicatorFlags
-{
-  kCSSymbolicatorTrackDyldActivity = 1,
 };
 
 struct _CSRange
@@ -104,9 +94,8 @@ GUM_DECLARE_CS_FUNC (Release, void, (CSTypeRef cs));
 
 GUM_DECLARE_CS_FUNC (SymbolicatorCreateWithPathAndArchitecture,
     CSSymbolicatorRef, (const char * path, cpu_type_t cpu_type));
-GUM_DECLARE_CS_FUNC (SymbolicatorCreateWithTaskFlagsAndNotification,
-    CSSymbolicatorRef, (task_t task, CSSymbolicatorFlags flags,
-    CSNotificationBlock notification));
+GUM_DECLARE_CS_FUNC (SymbolicatorCreateWithTask, CSSymbolicatorRef,
+    (task_t task));
 GUM_DECLARE_CS_FUNC (SymbolicatorGetSymbolWithAddressAtTime, CSSymbolRef,
     (CSSymbolicatorRef symbolicator, mach_vm_address_t address, CSTime time));
 GUM_DECLARE_CS_FUNC (SymbolicatorGetSourceInfoWithAddressAtTime,
@@ -193,11 +182,7 @@ gum_darwin_symbolicator_initable_init (GInitable * initable,
   }
   else
   {
-    self->handle = CSSymbolicatorCreateWithTaskFlagsAndNotification (self->task,
-        kCSSymbolicatorTrackDyldActivity,
-        ^(uint32_t notification_type, CSNotificationData data)
-        {
-        });
+    self->handle = CSSymbolicatorCreateWithTask (self->task);
     if (CSIsNull (self->handle))
       goto invalid_task;
   }
@@ -539,7 +524,7 @@ gum_cs_load_library (gpointer data)
   GUM_TRY_ASSIGN_CS_FUNC (Release);
 
   GUM_TRY_ASSIGN_CS_FUNC (SymbolicatorCreateWithPathAndArchitecture);
-  GUM_TRY_ASSIGN_CS_FUNC (SymbolicatorCreateWithTaskFlagsAndNotification);
+  GUM_TRY_ASSIGN_CS_FUNC (SymbolicatorCreateWithTask);
   GUM_TRY_ASSIGN_CS_FUNC (SymbolicatorGetSymbolWithAddressAtTime);
   GUM_TRY_ASSIGN_CS_FUNC (SymbolicatorGetSourceInfoWithAddressAtTime);
   GUM_TRY_ASSIGN_CS_FUNC (SymbolicatorForeachSymbolAtTime);
