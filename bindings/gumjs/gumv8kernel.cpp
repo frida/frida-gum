bbc7b8a3d140f0e5658e09004c0dd26713278cc9
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-02-21 21:58:45 +0100

    Fix Windows build regression

--- bindings/gumjs/gumv8kernel.cpp
+++ bindings/gumjs/gumv8kernel.cpp
@@ -691,12 +691,10 @@ gum_v8_kernel_write (GumMemoryValueType type,
         return;
 
       str_length = g_utf8_strlen (str, -1);
+
       if (type == GUM_MEMORY_VALUE_UTF16_STRING)
         str_utf16 = g_utf8_to_utf16 (str, -1, NULL, NULL, NULL);
-#ifdef G_OS_WIN32
-      else if (type == GUM_MEMORY_VALUE_ANSI_STRING)
-        str_ansi = gum_ansi_string_from_utf8 (str);
-#endif
+
       break;
     default:
       g_assert_not_reached ();
