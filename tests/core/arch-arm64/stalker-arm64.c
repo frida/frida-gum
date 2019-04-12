frida/frida-gum

commit 73bf271a4e2d4dc2f2e37b3a04bb3681e8160dd0

Fix Linux thread tampering in non-dumpable processes
Fixes frida#330 partially and frida#319.

gum/backend-linux/gumprocess-linux.c
@@ -20,6 +20,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <gio/gio.h>
+#include <sys/prctl.h>
 #include <sys/ptrace.h>
 #ifdef HAVE_ASM_PTRACE_H
 # include <asm/ptrace.h>
@@ -347,6 +348,7 @@ gum_process_modify_thread (GumThreadId thread_id,
     gssize child;
     gpointer stack, tls;
     GumUserDesc * desc;
+    int prev_dumpable;
 
     res = socketpair (AF_UNIX, SOCK_STREAM, 0, ctx.fd);
     g_assert_cmpint (res, ==, 0);
@@ -380,6 +382,17 @@ gum_process_modify_thread (GumThreadId thread_id,
 #else
     desc = tls;
 #endif
+
+    /*
+     * Some systems (notably Android on release applications) spawn processes as
+     * not dumpable by default, disabling ptrace() on that process for anyone
+     * other than root.
+     *
+     * To allow our child to ptrace() this process, we enable this temporarily.
+     */
+    prev_dumpable = prctl (PR_GET_DUMPABLE);
+    if (prev_dumpable != -1 && prev_dumpable != 1)
+      prctl (PR_SET_DUMPABLE, 1);
 
     /*
      * It seems like the only reliable way to read/write the registers of
@@ -438,6 +451,12 @@ gum_process_modify_thread (GumThreadId thread_id,
 
         success = gum_await_ack (fd, GUM_ACK_WROTE_CONTEXT);
       }
+    }
+
+    if (prev_dumpable != -1 && prev_dumpable != 1)
+    {
+      int res = prctl (PR_SET_DUMPABLE, prev_dumpable);
+      g_assert (res == 0);
     }
 
     waitpid (child, NULL, __WCLONE);

tests/core/arch-arm64/stalker-arm64.c
@@ -8,6 +8,7 @@
 #include "stalker-arm64-fixture.c"
 
 #include <lzma.h>
+#include <sys/prctl.h>
 
 TESTLIST_BEGIN (stalker)
 
@@ -1039,6 +1040,7 @@ TESTCASE (follow_thread)
   StalkerVictimContext ctx;
   GumThreadId thread_id;
   GThread * thread;
+  int prev_dumpable;
 
 #ifdef HAVE_ANDROID
   if (!g_test_slow ())
@@ -1047,6 +1049,10 @@ TESTCASE (follow_thread)
     return;
   }
 #endif
+
+  /* Android spawns non-debuggable applications as not dumpable by default. */
+  prev_dumpable = prctl (PR_GET_DUMPABLE);
+  prctl (PR_SET_DUMPABLE, 0);
 
   ctx.state = STALKER_VICTIM_CREATED;
   g_mutex_init (&ctx.mutex);
@@ -1105,6 +1111,8 @@ TESTCASE (follow_thread)
 
   g_mutex_clear (&ctx.mutex);
   g_cond_clear (&ctx.cond);
+
+  prctl (PR_SET_DUMPABLE, prev_dumpable);
 }
 
 TESTCASE (heap_api)

commit 3faa922e10b6fa1bfd5484cd69a2d5d8bc21c373

Fix non-Linux build regressions
Introduced in 73bf271a4e2d4dc2f2e37b3a04bb3681e8160dd0.

gum/backend-linux/gumprocess-linux.c
@@ -454,10 +454,7 @@ gum_process_modify_thread (GumThreadId thread_id,
     }
 
     if (prev_dumpable != -1 && prev_dumpable != 1)
-    {
-      int res = prctl (PR_SET_DUMPABLE, prev_dumpable);
-      g_assert (res == 0);
-    }
+      prctl (PR_SET_DUMPABLE, prev_dumpable);
 
     waitpid (child, NULL, __WCLONE);
 

tests/core/arch-arm64/stalker-arm64.c
@@ -8,7 +8,9 @@
 #include "stalker-arm64-fixture.c"
 
 #include <lzma.h>
-#include <sys/prctl.h>
+#ifdef HAVE_LINUX
+# include <sys/prctl.h>
+#endif
 
 TESTLIST_BEGIN (stalker)
 
@@ -1050,9 +1052,11 @@ TESTCASE (follow_thread)
   }
 #endif
 
+#ifdef HAVE_LINUX
   /* Android spawns non-debuggable applications as not dumpable by default. */
   prev_dumpable = prctl (PR_GET_DUMPABLE);
   prctl (PR_SET_DUMPABLE, 0);
+#endif
 
   ctx.state = STALKER_VICTIM_CREATED;
   g_mutex_init (&ctx.mutex);
@@ -1112,7 +1116,9 @@ TESTCASE (follow_thread)
   g_mutex_clear (&ctx.mutex);
   g_cond_clear (&ctx.cond);
 
+#ifdef HAVE_LINUX
   prctl (PR_SET_DUMPABLE, prev_dumpable);
+#endif
 }
 
 TESTCASE (heap_api)