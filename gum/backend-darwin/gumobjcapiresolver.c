2d7868eae5937958b97643908a0e2124683cd598
Ole André Vadla Ravnås <oleavr@gmail.com>
2019-05-04 02:21:23 +0200

    Update gumobjcapiresolver.c

--- gum/backend-darwin/gumobjcapiresolver.c
+++ gum/backend-darwin/gumobjcapiresolver.c
@@ -14,7 +14,7 @@
 #include <stdlib.h>
 
 typedef struct _GumObjcClassMetadata GumObjcClassMetadata;
-typedef void (* GumLibcFreeFunc) (void *);
+typedef void (* GumLibcFreeFunc) (gpointer mem);
 
 struct _GumObjcApiResolver
 {
