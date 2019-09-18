5dc181ac6592c23c2eeaac3a2cf914fee73f3f1a
Alvaro <alvaro.felipe91@gmail.com>
2019-09-18 20:03:31 +0200

    Handle multiple devices and linux host test for Android

--- tests/run-android-arm.sh
+++ tests/run-android-arm.sh
@@ -5,10 +5,12 @@ arch=arm
 remote_prefix=/data/local/tmp/frida-tests-$arch
 
 gum_tests=$(dirname "$0")
+. $gum_tests/android_devices.rc 
+. $gum_tests/configure_os.rc 
 cd "$gum_tests/../../build/tmp-android-$arch/frida-gum" || exit 1
-. ../../frida-meson-env-macos-x86_64.rc
+. ../../frida-meson-env-$os-x86_64.rc
 ninja || exit 1
 cd tests
-adb shell "mkdir $remote_prefix"
-adb push gum-tests data $remote_prefix || exit 1
-adb shell "$remote_prefix/gum-tests $@"
+adb -s $device shell "mkdir $remote_prefix"
+adb -s $device push gum-tests data $remote_prefix || exit 1
+adb -s $device shell "$remote_prefix/gum-tests $@"
