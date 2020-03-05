701588dcd9e4f9065af518e6efe84d8b96687213
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-03-05 03:58:11 +0100

    Improve the iOS test script
    Dump and symbolicate crash log, and target arm64e by default.

--- tests/run-ios.sh
+++ tests/run-ios.sh
@@ -1,15 +1,47 @@
 #!/bin/sh
 
-arch=arm64
+arch=arm64e
 
 remote_host=iphone
 remote_prefix=/usr/local/opt/frida-tests-$arch
 
 gum_tests=$(dirname "$0")
+
 cd "$gum_tests/../../build/tmp_thin-ios-$arch/frida-gum" || exit 1
+
 . ../../frida_thin-meson-env-macos-x86_64.rc
 ninja || exit 1
+
 cd tests
+
 ssh "$remote_host" "mkdir -p '$remote_prefix'"
 rsync -rLz gum-tests data "$remote_host:$remote_prefix/" || exit 1
-ssh "$remote_host" "$remote_prefix/gum-tests" "$@"
+
+ssh "$remote_host" "rm -f /var/mobile/Library/Logs/CrashReporter/gum-tests-*"
+
+log_path=$(mktemp "$TMPDIR/gum-tests.XXXXXX")
+ssh "$remote_host" "$remote_prefix/gum-tests" "$@" | tee "$log_path"
+
+if [ ${PIPESTATUS[0]} -ne 0 ]; then
+  while ! ssh "$remote_host" "ls /var/mobile/Library/Logs/CrashReporter/gum-tests-*" 2>/dev/null; do
+    sleep 1
+  done
+
+  remote_report_path=$(ssh "$remote_host" "echo /var/mobile/Library/Logs/CrashReporter/gum-tests-*")
+  local_report_path=$(mktemp "$TMPDIR/gum-tests.XXXXXX")
+  scp "$remote_host:$remote_report_path" "$local_report_path"
+
+  program_base=$(egrep "^(0x.+)\sgum-tests" "$local_report_path" | awk '{ print $1; }')
+
+  cat "$local_report_path"
+
+  echo "Crash:"
+  egrep "^\d+\s+gum-tests" "$local_report_path" \
+    | awk '{ print $3; }' \
+    | xargs atos -o gum-tests -l $program_base
+
+  rm "$local_report_path"
+  ssh "$remote_host" "rm $remote_report_path"
+fi
+
+rm "$log_path"
