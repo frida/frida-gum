8558f7d0954c64d7339325f065a6755d61e76da7
jonwilson030981 <jonwilson@zepler.net>
2020-03-26 10:26:23 +0000

    Changes to use inline assembly in tests

--- tests/core/arch-arm/stalker-arm.c
+++ tests/core/arch-arm/stalker-arm.c
@@ -8,6 +8,7 @@
 #include "stalker-arm-fixture.c"
 
 TESTLIST_BEGIN (stalker)
+  TESTENTRY (flat_code)
   TESTENTRY (no_events)
   TESTENTRY (trust_is_zero)
   TESTENTRY (trust_unsupported)
@@ -25,14 +26,20 @@ TESTLIST_END ()
 
 gint gum_stalker_dummy_global_to_trick_optimizer = 0;
 
-static const guint32 flat_code[] = {
-  0xe0400000, /* SUB R0, R0, R0 */
-  0xe2800001, /* ADD R0, R0, #1 */
-  0xe2800001, /* ADD R0, R0, #1 */
-  0xe1a0f00e  /* MOV PC, LR     */
-};
 
-#define FLAT_CODE_INSN_COUNT (sizeof(flat_code)/sizeof(guint32))
+extern const void flat_code;
+extern const void flat_code_end;
+
+asm (
+  "flat_code: \n"
+  "sub r0, r0, r0 \n"
+  "add r0, r0, #1 \n"
+  "add r0, r0, #1 \n"
+  "mov pc, lr \n"
+  "flat_code_end: \n"
+);
+
+#define FLAT_CODE_INSN_COUNT ((&flat_code_end - &flat_code)/sizeof(guint32))
 
 static StalkerTestFunc
 invoke_expecting_return_value (TestArmStalkerFixture * fixture,
@@ -59,9 +66,9 @@ invoke_flat_expecting_return_value (TestArmStalkerFixture * fixture,
                                     GumEventType mask,
                                     guint expected_return_value)
 {
-  return invoke_expecting_return_value(fixture, mask, flat_code,
-                                       sizeof(flat_code),
-                                      expected_return_value);
+  return invoke_expecting_return_value(fixture, mask, &flat_code,
+                                       &flat_code_end - &flat_code,
+                                       expected_return_value);
 }
 
 static StalkerTestFunc
@@ -69,6 +76,16 @@ invoke_flat (TestArmStalkerFixture * fixture,
              GumEventType mask)
 {
   return invoke_flat_expecting_return_value (fixture, mask, 2);
+}
+
+TESTCASE (flat_code)
+{
+  g_assert_cmpuint ((&flat_code_end - &flat_code), ==, 16);
+  guint* code = (guint*)&flat_code;
+  g_assert_cmpuint(code[0], ==, 0xe0400000);
+  g_assert_cmpuint(code[1], ==, 0xe2800001);
+  g_assert_cmpuint(code[2], ==, 0xe2800001);
+  g_assert_cmpuint(code[3], ==, 0xe1a0f00e);
 }
 
 TESTCASE (no_events)
@@ -219,10 +236,12 @@ TESTCASE (block_events_generated)
   GUM_ASSERT_CMPADDR (ev->end, ==, func + (3 * 4));
 }
 
-// Test we call virtualize bl/blr
+// Move assembly code into __asm__ blocks
+
+// Compare test list to aarch64
 // Test we can emit events for ret
-// Test we can virtualize ret
-// Test we can emit events for block
-// Add GumExecFrame when generating call and check call depth.
+// Test calling mulitple levels deep and check call depth
+// Check thumb/jazelle is excluded.
+// Test adding excluded ranges
 // Test we can unfollow (move check to virtualize funcs)
 // Test conditional calls and jmps
