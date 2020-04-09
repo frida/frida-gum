7a5ac89096071d31b7916fe7f27275257f7875d6
Francesco Tamagni <mrmacete@protonmail.ch>
2020-04-09 21:16:50 +0200

    Rename all variables

--- bindings/gumjs/runtime/cmodule/ptrauth.h
+++ bindings/gumjs/runtime/cmodule/ptrauth.h
@@ -24,13 +24,13 @@ typedef uintptr_t ptrauth_generic_signature_t;
 #ifdef HAVE_PTRAUTH
 
 # define ptrauth_strip(value, key) \
-    ((__key <= ptrauth_key_asib) ? __arm64_ptrauth_strip_i (value)\
+    ((key <= ptrauth_key_asib) ? __arm64_ptrauth_strip_i (value)\
         : __arm64_ptrauth_strip_d (value))
 # define ptrauth_blend_discriminator(pointer, integer) \
     ((void *) (size_t) (((size_t) (void *) (pointer) & 0x0000ffffffffffffULL) |\
         (((size_t) integer & 0xffffULL) << 48)))
 # define ptrauth_sign_unauthenticated(value, key, data) \
-    ((__key == ptrauth_key_asia) ? __arm64_ptrauth_sign_ia (value, data)\
+    ((key == ptrauth_key_asia) ? __arm64_ptrauth_sign_ia (value, data)\
         : (key == ptrauth_key_asib) ? __arm64_ptrauth_sign_ib (value, data)\
         : (key == ptrauth_key_asda) ? __arm64_ptrauth_sign_da (value, data)\
         : (key == ptrauth_key_asdb) ? __arm64_ptrauth_sign_db (value, data)\
@@ -38,8 +38,8 @@ typedef uintptr_t ptrauth_generic_signature_t;
 
 #else
 
-# define ptrauth_strip(__value, __key) __value
-# define ptrauth_blend_discriminator(__pointer, __integer) ((uintptr_t) 0)
-# define ptrauth_sign_unauthenticated(__value, __key, __data) __value
+# define ptrauth_strip(value, key) value
+# define ptrauth_blend_discriminator(pointer, integer) ((uintptr_t) 0)
+# define ptrauth_sign_unauthenticated(value, key, data) value
 
 #endif
