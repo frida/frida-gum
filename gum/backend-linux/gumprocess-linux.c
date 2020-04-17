cf95fcf6c422baf732c0ebcbdbf5b8b8615a5a24
WorksButNotTested <62701594+WorksButNotTested@users.noreply.github.com>
2020-04-17 18:48:50 +0100

    Update gum/backend-linux/gumprocess-linux.c
    Co-Authored-By: Ole André Vadla Ravnås <oleavr@gmail.com>

--- gum/backend-linux/gumprocess-linux.c
+++ gum/backend-linux/gumprocess-linux.c
@@ -927,6 +927,7 @@ gum_handle_interpreter (const GumModuleDetails * details,
     if (contents[i] == '\0')
     {
       GumEmitExecutableModuleContext emc;
+
       emc.executable_path = &contents[i + 1];
       emc.func = ctx->func;
       emc.user_data = ctx->user_data;
