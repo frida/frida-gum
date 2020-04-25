f7c633731a12229c6d082d3e3417c27cb5fe241f
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-04-25 08:42:23 +0200

    Update sign.sh to use MACOS_CERTID like elsewhere

--- tests/sign.sh
+++ tests/sign.sh
@@ -12,8 +12,8 @@ fi
 
 case $host_os in
   macos)
-    if [ -z "$MAC_CERTID" ]; then
-      echo "MAC_CERTID not set, see https://github.com/frida/frida#macos-and-ios"
+    if [ -z "$MACOS_CERTID" ]; then
+      echo "MACOS_CERTID not set, see https://github.com/frida/frida#macos-and-ios"
       exit 1
     fi
     ;;
@@ -33,7 +33,7 @@ cp "$runner_binary" "$signed_runner_binary"
 
 case $host_os in
   macos)
-    "$CODESIGN" -f -s "$MAC_CERTID" -i "re.frida.GumTests" "$signed_runner_binary" || exit 1
+    "$CODESIGN" -f -s "$MACOS_CERTID" -i "re.frida.GumTests" "$signed_runner_binary" || exit 1
     ;;
   ios)
     "$CODESIGN" -f -s "$IOS_CERTID" --entitlements "$runner_entitlements" "$signed_runner_binary" || exit 1
