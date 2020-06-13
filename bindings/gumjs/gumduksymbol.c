e7f9996189ed9427e0c15b2a90af6187a39de440
Matt Oh <oh.jeongwook@gmail.com>
2020-06-12 17:57:27 -0700

    Update bindings/gumjs/gumduksymbol.c
    Co-authored-by: Ole André Vadla Ravnås <oleavr@gmail.com>

--- bindings/gumjs/gumduksymbol.c
+++ bindings/gumjs/gumduksymbol.c
@@ -194,7 +194,7 @@ GUMJS_DEFINE_FUNCTION (gumjs_symbol_load)
 
   _gum_duk_scope_suspend (&scope);
   success = gum_load_symbols(path);
-  _gum_duk_scope_resume(&scope);
+  _gum_duk_scope_resume (&scope);
 
   duk_push_boolean(ctx, success);
   return 1;
