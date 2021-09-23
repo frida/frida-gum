db101c149600495d4bceaf8ddb7ba2008d464f4e
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-09-23 19:56:23 +0200

    gumjs: Add missing G_GNUC_INTERNAL declaration

--- bindings/gumjs/gumv8value.h
+++ bindings/gumjs/gumv8value.h
@@ -118,8 +118,8 @@ G_GNUC_INTERNAL void _gum_v8_parse_exception_details (
     GumExceptionDetails * details, v8::Local<v8::Object> & exception,
     v8::Local<v8::Object> & cpu_context, GumV8Core * core);
 
-v8::Local<v8::Value> _gum_v8_error_new_take_error (v8::Isolate * isolate,
-    GError ** error);
+G_GNUC_INTERNAL v8::Local<v8::Value> _gum_v8_error_new_take_error (
+    v8::Isolate * isolate, GError ** error);
 
 G_GNUC_INTERNAL const gchar * _gum_v8_thread_state_to_string (
     GumThreadState state);
