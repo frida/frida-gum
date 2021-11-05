d861e97a157238e94c20ca9adba03db1a28d2510
WorksButNotTested <62701594+WorksButNotTested@users.noreply.github.com>
2021-11-06 00:00:40 +0100

    x86-reader: Expose disassemble_instruction_at()

--- gum/arch-x86/gumx86reader.h
+++ gum/arch-x86/gumx86reader.h
@@ -19,6 +19,7 @@ gboolean gum_x86_reader_insn_is_jcc (const cs_insn * insn);
 gpointer gum_x86_reader_try_get_relative_call_target (gconstpointer address);
 gpointer gum_x86_reader_try_get_relative_jump_target (gconstpointer address);
 gpointer gum_x86_reader_try_get_indirect_jump_target (gconstpointer address);
+cs_insn * gum_x86_reader_disassemble_instruction_at (gconstpointer address);
 
 G_END_DECLS
 
