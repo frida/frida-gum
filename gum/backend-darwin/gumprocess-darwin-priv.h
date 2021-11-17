8e365e8d79914057c2f43fbf61f9b01665c72380
Francesco Tamagni <mrmacete@protonmail.ch>
2021-11-17 14:51:54 +0100

    Make header self-contained and add license

--- gum/backend-darwin/gumprocess-darwin-priv.h
+++ gum/backend-darwin/gumprocess-darwin-priv.h
@@ -1,5 +1,13 @@
+/*
+ * Copyright (C) 2021 Francesco Tamagni <mrmacete@protonmail.ch>
+ *
+ * Licence: wxWindows Library Licence, Version 3.1
+ */
+
 #ifndef __GUM_PROCESS_DARWIN_PRIV_H__
 #define __GUM_PROCESS_DARWIN_PRIV_H__
+
+#include <glib.h>
 
 #define DYLD_INFO_COUNT 5
 #define DYLD_INFO_LEGACY_COUNT 1
