fdd1c0933123ab67c2c9d5551d42a56ac53a69f9
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-11-30 23:56:20 +0100

    Tweak style

--- gum/backend-darwin/gumobjcdisposeclasspairmonitor.c
+++ gum/backend-darwin/gumobjcdisposeclasspairmonitor.c
@@ -12,8 +12,11 @@ static void gum_objc_dispose_class_pair_monitor_iface_init (gpointer g_iface,
     gpointer iface_data);
 static void gum_objc_dispose_class_pair_monitor_dispose (GObject * object);
 static void gum_objc_dispose_class_pair_monitor_finalize (GObject * object);
-static void the_monitor_weak_notify (gpointer data,
-    GObject * where_the_object_was);
+static void gum_on_weak_notify (gpointer data, GObject * where_the_object_was);
+static void gum_objc_dispose_class_pair_monitor_on_enter (
+    GumInvocationListener * listener, GumInvocationContext * context);
+static void gum_objc_dispose_class_pair_monitor_on_leave (
+    GumInvocationListener * listener, GumInvocationContext * context);
 
 G_DEFINE_TYPE_EXTENDED (GumObjcDisposeClassPairMonitor,
                         gum_objc_dispose_class_pair_monitor,
@@ -25,64 +28,14 @@ G_DEFINE_TYPE_EXTENDED (GumObjcDisposeClassPairMonitor,
 static GMutex _gum_objc_dispose_class_pair_monitor_lock;
 static GumObjcDisposeClassPairMonitor * _the_monitor = NULL;
 
-GumObjcDisposeClassPairMonitor *
-gum_objc_dispose_class_pair_monitor_obtain (void)
-{
-  GumObjcDisposeClassPairMonitor * monitor;
-
-  g_mutex_lock (&_gum_objc_dispose_class_pair_monitor_lock);
-
-  if (_the_monitor != NULL)
-  {
-    monitor = GUM_OBJC_DISPOSE_CLASS_PAIR_MONITOR (
-        g_object_ref (_the_monitor));
-  }
-  else
-  {
-    _the_monitor = g_object_new (GUM_TYPE_OBJC_DISPOSE_CLASS_PAIR_MONITOR,
-        NULL);
-    g_object_weak_ref (G_OBJECT (_the_monitor),
-        the_monitor_weak_notify, NULL);
-
-    monitor = _the_monitor;
-  }
-
-  g_mutex_unlock (&_gum_objc_dispose_class_pair_monitor_lock);
-
-  return monitor;
-}
-
 static void
-the_monitor_weak_notify (gpointer data,
-                         GObject * where_the_object_was)
+gum_objc_dispose_class_pair_monitor_class_init (
+    GumObjcDisposeClassPairMonitorClass * klass)
 {
-  g_mutex_lock (&_gum_objc_dispose_class_pair_monitor_lock);
+  GObjectClass * object_class = G_OBJECT_CLASS (klass);
 
-  g_assert (_the_monitor == (GumObjcDisposeClassPairMonitor *)
-      where_the_object_was);
-  _the_monitor = NULL;
-
-  g_mutex_unlock (&_gum_objc_dispose_class_pair_monitor_lock);
-}
-
-static void
-gum_objc_dispose_class_pair_monitor_on_enter (GumInvocationListener * listener,
-                                              GumInvocationContext * context)
-{
-  GumObjcDisposeClassPairMonitor * self =
-      GUM_OBJC_DISPOSE_CLASS_PAIR_MONITOR (listener);
-
-  g_rec_mutex_lock (&self->mutex);
-}
-
-static void
-gum_objc_dispose_class_pair_monitor_on_leave (GumInvocationListener * listener,
-                                              GumInvocationContext * context)
-{
-  GumObjcDisposeClassPairMonitor * self =
-      GUM_OBJC_DISPOSE_CLASS_PAIR_MONITOR (listener);
-
-  g_rec_mutex_unlock (&self->mutex);
+  object_class->dispose = gum_objc_dispose_class_pair_monitor_dispose;
+  object_class->finalize = gum_objc_dispose_class_pair_monitor_finalize;
 }
 
 static void
@@ -96,29 +49,18 @@ gum_objc_dispose_class_pair_monitor_iface_init (gpointer g_iface,
 }
 
 static void
-gum_objc_dispose_class_pair_monitor_class_init (
-    GumObjcDisposeClassPairMonitorClass * klass)
+gum_objc_dispose_class_pair_monitor_init (GumObjcDisposeClassPairMonitor * self)
 {
-  GObjectClass * object_class = G_OBJECT_CLASS (klass);
-
-  object_class->dispose = gum_objc_dispose_class_pair_monitor_dispose;
-  object_class->finalize = gum_objc_dispose_class_pair_monitor_finalize;
-}
-
-static void
-gum_objc_dispose_class_pair_monitor_init (
-    GumObjcDisposeClassPairMonitor * self)
-{
-  gpointer objc_disposeClassPair;
+  gpointer dispose_impl;
 
   g_rec_mutex_init (&self->mutex);
 
-  objc_disposeClassPair = GSIZE_TO_POINTER (gum_module_find_export_by_name (
+  dispose_impl = GSIZE_TO_POINTER (gum_module_find_export_by_name (
       "/usr/lib/libobjc.A.dylib", "objc_disposeClassPair"));
-  g_assert (objc_disposeClassPair != NULL);
+  g_assert (dispose_impl != NULL);
 
   self->interceptor = gum_interceptor_obtain ();
-  gum_interceptor_attach (self->interceptor, objc_disposeClassPair,
+  gum_interceptor_attach (self->interceptor, dispose_impl,
       GUM_INVOCATION_LISTENER (self), NULL);
 }
 
@@ -148,5 +90,63 @@ gum_objc_dispose_class_pair_monitor_finalize (GObject * object)
 
   G_OBJECT_CLASS (
       gum_objc_dispose_class_pair_monitor_parent_class)->finalize (object);
+}
+
+GumObjcDisposeClassPairMonitor *
+gum_objc_dispose_class_pair_monitor_obtain (void)
+{
+  GumObjcDisposeClassPairMonitor * monitor;
+
+  g_mutex_lock (&_gum_objc_dispose_class_pair_monitor_lock);
+
+  if (_the_monitor != NULL)
+  {
+    monitor = GUM_OBJC_DISPOSE_CLASS_PAIR_MONITOR (g_object_ref (_the_monitor));
+  }
+  else
+  {
+    _the_monitor = g_object_new (GUM_TYPE_OBJC_DISPOSE_CLASS_PAIR_MONITOR,
+        NULL);
+    g_object_weak_ref (G_OBJECT (_the_monitor), gum_on_weak_notify, NULL);
+
+    monitor = _the_monitor;
+  }
+
+  g_mutex_unlock (&_gum_objc_dispose_class_pair_monitor_lock);
+
+  return monitor;
+}
+
+static void
+gum_on_weak_notify (gpointer data,
+                    GObject * where_the_object_was)
+{
+  g_mutex_lock (&_gum_objc_dispose_class_pair_monitor_lock);
+
+  g_assert (_the_monitor == (GumObjcDisposeClassPairMonitor *)
+      where_the_object_was);
+  _the_monitor = NULL;
+
+  g_mutex_unlock (&_gum_objc_dispose_class_pair_monitor_lock);
+}
+
+static void
+gum_objc_dispose_class_pair_monitor_on_enter (GumInvocationListener * listener,
+                                              GumInvocationContext * context)
+{
+  GumObjcDisposeClassPairMonitor * self =
+      GUM_OBJC_DISPOSE_CLASS_PAIR_MONITOR (listener);
+
+  g_rec_mutex_lock (&self->mutex);
+}
+
+static void
+gum_objc_dispose_class_pair_monitor_on_leave (GumInvocationListener * listener,
+                                              GumInvocationContext * context)
+{
+  GumObjcDisposeClassPairMonitor * self =
+      GUM_OBJC_DISPOSE_CLASS_PAIR_MONITOR (listener);
+
+  g_rec_mutex_unlock (&self->mutex);
 }
 
