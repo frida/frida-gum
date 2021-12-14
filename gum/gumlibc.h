d62725c79c49f64276059b6a6d46cd1a82ad9651
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-12-14 18:45:57 +0100

    libc: Make symbols visible
    So bindings are able to use them when built as a shared library.

--- gum/gumlibc.h
+++ gum/gumlibc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2015-2021 Ole André Vadla Ravnås <oleavr@nowsecure.com>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
@@ -11,9 +11,9 @@
 
 G_BEGIN_DECLS
 
-G_GNUC_INTERNAL gpointer gum_memset (gpointer dst, gint c, gsize n);
-G_GNUC_INTERNAL gpointer gum_memcpy (gpointer dst, gconstpointer src, gsize n);
-G_GNUC_INTERNAL gpointer gum_memmove (gpointer dst, gconstpointer src, gsize n);
+GUM_API gpointer gum_memset (gpointer dst, gint c, gsize n);
+GUM_API gpointer gum_memcpy (gpointer dst, gconstpointer src, gsize n);
+GUM_API gpointer gum_memmove (gpointer dst, gconstpointer src, gsize n);
 
 G_END_DECLS
 
