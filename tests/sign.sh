d8119ce36b9d875c92a9a0a7acc68db572644584
Ole André Vadla Ravnås <oleavr@gmail.com>
2021-12-22 13:58:36 +0100

    tests: Improve sign.sh to not require any env vars
    We can always assume `codesign` is on the user's `PATH`, and use ad-hoc
    signing if an identity isn't specified.

--- tests/sign.sh
+++ tests/sign.sh
@@ -6,28 +6,16 @@ runner_entitlements="$3"
 signed_runner_binary="$4"
 
 if [ -z "$CODESIGN" ]; then
-  echo "CODESIGN not set"
-  exit 1
+  CODESIGN=codesign
 fi
 
-case $host_os in
-  macos)
-    if [ -z "$MACOS_CERTID" ]; then
-      echo "MACOS_CERTID not set, see https://github.com/frida/frida#macos-and-ios"
-      exit 1
-    fi
-    ;;
-  ios)
-    if [ -z "$IOS_CERTID" ]; then
-      echo "IOS_CERTID not set, see https://github.com/frida/frida#macos-and-ios"
-      exit 1
-    fi
-    ;;
-  *)
-    echo "Unexpected host OS"
-    exit 1
-    ;;
-esac
+if [ -z "$MACOS_CERTID" ]; then
+  MACOS_CERTID="-"
+fi
+
+if [ -z "$IOS_CERTID" ]; then
+  IOS_CERTID="-"
+fi
 
 rm -f "$signed_runner_binary"
 cp "$runner_binary" "$signed_runner_binary"
