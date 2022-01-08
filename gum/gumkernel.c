7f9a90b0d814e7838a0a8843991eba6e970a22fe
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-01-08 04:40:58 +0100

    kernel: Add missing annotations and some docs

--- gum/gumkernel.c
+++ gum/gumkernel.c
@@ -6,6 +6,47 @@
 
 #include "gumkernel.h"
 
+/**
+ * gum_kernel_scan:
+ * @range: the #GumMemoryRange to scan
+ * @pattern: the #GumMatchPattern to look for occurrences of
+ * @func: (scope call): function to process each match
+ * @user_data: data to pass to @func
+ *
+ * Scans the specified kernel memory @range for occurrences of @pattern,
+ * calling @func with each match.
+ */
+
+/**
+ * gum_kernel_enumerate_ranges:
+ * @prot: bitfield specifying the minimum protection
+ * @func: (scope call): function called with #GumRangeDetails
+ * @user_data: data to pass to @func
+ *
+ * Enumerates kernel memory ranges satisfying @prot, calling @func with
+ * #GumRangeDetails about each such range found.
+ */
+
+/**
+ * gum_kernel_enumerate_module_ranges:
+ * @module_name: (nullable): name of module, or %NULL for the kernel itself
+ * @prot: bitfield specifying the minimum protection
+ * @func: (scope call): function called with #GumKernelModuleRangeDetails
+ * @user_data: data to pass to @func
+ *
+ * Enumerates kernel memory ranges of the specified module that satisfy @prot,
+ * calling @func with #GumKernelModuleRangeDetails about each such range found.
+ */
+
+/**
+ * gum_kernel_enumerate_modules:
+ * @func: (scope call): function called with #GumModuleDetails
+ * @user_data: data to pass to @func
+ *
+ * Enumerates kernel modules loaded right now, calling @func with
+ * #GumModuleDetails about each module found.
+ */
+
 #ifndef HAVE_DARWIN
 
 gboolean
