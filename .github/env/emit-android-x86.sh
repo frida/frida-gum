717059c12a1cbc4c6a21c4b80b75eaecfccbca84
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-03-17 11:15:54 +0100

    ci: Build and run tests on Android/x86

---
+++ .github/env/emit-android-x86.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+pkgconfig=$1
+needs_exe_wrapper=$2
+
+bindir=$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-$(uname -m)/bin
+
+cat << EOF
+[constants]
+c_like_flags = ['-DANDROID', '-march=pentium4', '-mfpmath=sse', '-mstackrealign', '-ffunction-sections', '-fdata-sections']
+linker_flags = ['-Wl,--gc-sections', '-Wl,-z,noexecstack', '-Wl,-z,relro', '-Wl,-z,now']
+
+[binaries]
+c = '$bindir/i686-linux-android19-clang'
+cpp = '$bindir/i686-linux-android19-clang++'
+strip = '$bindir/llvm-strip'
+pkgconfig = '$pkgconfig'
+
+[built-in options]
+c_args = c_like_flags
+cpp_args = c_like_flags
+c_link_args = linker_flags
+cpp_link_args = linker_flags + ['-static-libstdc++']
+
+[host_machine]
+system = 'android'
+cpu_family = 'x86'
+cpu = 'i686'
+endian = 'little'
+
+[properties]
+needs_exe_wrapper = $needs_exe_wrapper
+EOF
