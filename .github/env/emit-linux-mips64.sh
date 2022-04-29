aa174b75d42a4b09da9ac882d3266bf9c8c6da7a
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-04-29 22:07:59 +0000

    ci: Add CI for Linux/MIPS

---
+++ .github/env/emit-linux-mips64.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+pkgconfig=$1
+needs_exe_wrapper=$2
+
+toolpfx=mips64-linux-gnuabi64-
+
+cat << EOF
+[constants]
+base_flags = ['-march=mips64r2', '-mabi=64']
+c_like_flags = ['-ffunction-sections', '-fdata-sections']
+linker_flags = ['-static-libgcc', '-Wl,--gc-sections', '-Wl,-z,noexecstack']
+
+[binaries]
+c = '${toolpfx}gcc'
+cpp = '${toolpfx}g++'
+ar = '${toolpfx}ar'
+nm = '${toolpfx}nm'
+readelf = '${toolpfx}readelf'
+strip = '${toolpfx}strip'
+pkgconfig = '$pkgconfig'
+
+[built-in options]
+c_args = base_flags + c_like_flags
+cpp_args = base_flags + c_like_flags
+c_link_args = base_flags + linker_flags
+cpp_link_args = base_flags + linker_flags + ['-static-libstdc++']
+
+[host_machine]
+system = 'linux'
+cpu_family = 'mips64'
+cpu = 'mips64r2'
+endian = 'big'
+
+[properties]
+needs_exe_wrapper = $needs_exe_wrapper
+EOF
