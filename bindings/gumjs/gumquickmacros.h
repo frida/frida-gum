640f66c15a4760a49270c6e8efc6f6cf4f7d2419
Ole André Vadla Ravnås <oleavr@gmail.com>
2022-05-26 13:24:36 +0200

    gumjs: Update outdated copyright header

--- bindings/gumjs/gumquickmacros.h
+++ bindings/gumjs/gumquickmacros.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2020-2022 Ole André Vadla Ravnås <oleavr@nowsecure.com>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
