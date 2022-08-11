199091df563610b9c9b49855f4d7572fcd11d1be
WorksButNotTested <62701594+WorksButNotTested@users.noreply.github.com>
2022-08-11 17:47:56 +0100

    stalker: Apply ARM backpatches when notified

--- gum/backend-arm/gumstalker-arm.c
+++ gum/backend-arm/gumstalker-arm.c
@@ -541,6 +541,13 @@ static GumCalloutEntry * gum_exec_block_get_last_callout_entry (
 static void gum_exec_block_set_last_callout_entry (GumExecBlock * block,
     GumCalloutEntry * entry);
 
+static void gum_exec_ctx_backpatch_arm_branch_to_current (GumExecBlock * block,
+    GumExecBlock * from, gpointer from_insn, gsize code_offset,
+    GumPrologState opened_prolog);
+static void gum_exec_ctx_backpatch_thumb_branch_to_current (
+    GumExecBlock * block, GumExecBlock * from, gpointer from_insn,
+    gsize code_offset, GumPrologState opened_prolog);
+
 static void gum_exec_block_virtualize_arm_branch_insn (GumExecBlock * block,
     const GumBranchTarget * target, arm_cc cc, GumWriteback * writeback,
     GumGeneratorContext * gc);
@@ -1254,6 +1261,40 @@ void
 gum_stalker_prefetch_backpatch (GumStalker * self,
                                 const GumBackpatch * backpatch)
 {
+  GumExecCtx * ctx;
+  GumExecBlock * block_to, * block_from;
+  gpointer code_address_to, code_address_from;
+  gpointer from_insn = backpatch->from_insn;
+
+  ctx = gum_stalker_get_exec_ctx ();
+  g_assert (ctx != NULL);
+
+  block_to = gum_exec_ctx_obtain_block_for (ctx, backpatch->to,
+      &code_address_to);
+  block_from = gum_exec_ctx_obtain_block_for (ctx, backpatch->from,
+      &code_address_from);
+
+  block_to->recycle_count = self->trust_threshold;
+  block_from->recycle_count = self->trust_threshold;
+
+  switch (backpatch->type)
+  {
+    case GUM_BACKPATCH_ARM:
+    {
+      gum_exec_ctx_backpatch_arm_branch_to_current (block_to, block_from,
+          from_insn, backpatch->code_offset, backpatch->opened_prolog);
+      break;
+    }
+    case GUM_BACKPATCH_THUMB:
+    {
+      gum_exec_ctx_backpatch_thumb_branch_to_current (block_to, block_from,
+          from_insn, backpatch->code_offset, backpatch->opened_prolog);
+      break;
+    }
+    default:
+      g_assert_not_reached ();
+      break;
+  }
 }
 
 void
@@ -1276,13 +1317,13 @@ gum_stalker_recompile (GumStalker * self,
 gpointer
 gum_stalker_backpatch_get_from (const GumBackpatch * backpatch)
 {
-  return NULL;
+  return backpatch->from;
 }
 
 gpointer
 gum_stalker_backpatch_get_to (const GumBackpatch * backpatch)
 {
-  return NULL;
+  return backpatch->to;
 }
 
 void
