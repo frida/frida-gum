ce350bca8ebed935fc94a37adf80485cc65f6091
Ole André Vadla Ravnås <oleavr@gmail.com>
2023-03-30 13:25:35 +0200

    process: Add prototype for _enumerate_modules()

--- gum/gumprocess-priv.h
+++ gum/gumprocess-priv.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2017-2023 Ole André Vadla Ravnås <oleavr@nowsecure.com>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
@@ -12,6 +12,8 @@
 G_BEGIN_DECLS
 
 G_GNUC_INTERNAL void _gum_process_enumerate_threads (GumFoundThreadFunc func,
+    gpointer user_data);
+G_GNUC_INTERNAL void _gum_process_enumerate_modules (GumFoundModuleFunc func,
     gpointer user_data);
 G_GNUC_INTERNAL void _gum_process_enumerate_ranges (GumPageProtection prot,
     GumFoundRangeFunc func, gpointer user_data);
