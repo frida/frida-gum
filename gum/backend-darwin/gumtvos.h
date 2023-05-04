d6d89d45d3607af84e7e83861b8aa3a1ebde5a9a
Aman Karmani <aman@tmm1.net>
2023-05-04 11:34:13 -0700

    style

--- gum/backend-darwin/gumtvos.h
+++ gum/backend-darwin/gumtvos.h
@@ -2,14 +2,14 @@
 #define __GUMTVOS_H__
 
 #ifdef HAVE_TVOS
-#include <Availability.h>
+# include <Availability.h>
 #undef __TVOS_PROHIBITED
 #define __TVOS_PROHIBITED
 #undef __API_UNAVAILABLE
 #define __API_UNAVAILABLE(...)
-#include <spawn.h>
-#include <mach/mach.h>
-#include <mach/task.h>
+# include <spawn.h>
+# include <mach/mach.h>
+# include <mach/task.h>
 #undef __TVOS_PROHIBITED
 #define __TVOS_PROHIBITED __OS_AVAILABILITY(tvos,unavailable)
 #endif
