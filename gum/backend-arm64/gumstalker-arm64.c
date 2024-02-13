8d2addd1f93d3775dfd02be360eb916e84c94e06
s1341 <github@shmarya.net>
2024-02-13 14:37:52 +0200

    stalker-arm64: get rid of unnecessary and undefined function

--- gum/backend-arm64/gumstalker-arm64.c
+++ gum/backend-arm64/gumstalker-arm64.c
@@ -3030,8 +3030,6 @@ gum_stalker_iterator_put_chaining_return (GumStalkerIterator * self)
   if ((block->ctx->sink_mask & GUM_RET) != 0)
     gum_exec_block_write_ret_event_code (block, gc, GUM_CODE_INTERRUPTIBLE);
 
-  gum_exec_block_write_adjust_depth (block, gc->code_writer, -1);
-
   gum_exec_block_write_chaining_return_code (block, gc, ARM64_REG_LR);
 }
 
