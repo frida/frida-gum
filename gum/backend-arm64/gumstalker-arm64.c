36452418c1f66306a11599533aa644ae8a90a6ee
s1341 <github@shmarya.net>
2024-02-13 14:56:44 +0200

    stalker-arm64: use X30 instead of LR

--- gum/backend-arm64/gumstalker-arm64.c
+++ gum/backend-arm64/gumstalker-arm64.c
@@ -3030,7 +3030,7 @@ gum_stalker_iterator_put_chaining_return (GumStalkerIterator * self)
   if ((block->ctx->sink_mask & GUM_RET) != 0)
     gum_exec_block_write_ret_event_code (block, gc, GUM_CODE_INTERRUPTIBLE);
 
-  gum_exec_block_write_chaining_return_code (block, gc, ARM64_REG_LR);
+  gum_exec_block_write_chaining_return_code (block, gc, ARM64_REG_X30);
 }
 
 GumMemoryAccess
