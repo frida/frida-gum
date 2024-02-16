5d8bcfeac0f4f6a83641dba15d297c05f9802c2c
Francesco Tamagni <mrmacete@protonmail.ch>
2024-02-16 11:41:17 +0100

    interceptor: Add translate_return_address

--- gum/guminterceptor.c
+++ gum/guminterceptor.c
@@ -844,6 +844,28 @@ fallback:
   return return_address;
 }
 
+gpointer
+gum_interceptor_translate_return_address (gpointer return_address)
+{
+  GumInvocationStack * stack;
+  GumInvocationStackEntry * entry;
+  gint i;
+
+  stack = gum_interceptor_get_current_stack ();
+  if (stack->len == 0)
+    goto fallback;
+
+  for (i = stack->len - 1; i >= 0; i--)
+  {
+    entry = &g_array_index (stack, GumInvocationStackEntry, i);
+    if (entry->function_ctx->on_leave_trampoline == return_address)
+      return entry->caller_ret_addr;
+  }
+
+fallback:
+  return return_address;
+}
+
 static GumFunctionContext *
 gum_interceptor_instrument (GumInterceptor * self,
                             GumInterceptorType type,
