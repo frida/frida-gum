f147df7552ed5ada91c56bb0ae321b0b972323cf
Francesco Tamagni <mrmacete@protonmail.ch>
2024-03-27 16:49:35 +0100

    spinlock: Add try_acquire()

--- gum/backend-x86/gumspinlock-ia32-msc.c
+++ gum/backend-x86/gumspinlock-ia32-msc.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010-2019 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2024 Francesco Tamagni <mrmacete@protonmail.ch>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
@@ -33,6 +34,19 @@ try_again:
 beach:
     ret;
   }
+}
+
+gboolean
+gum_spinlock_try_acquire (GumSpinlock * spinlock)
+{
+  volatile guint32 is_held = *(guint32 *) spinlock;
+
+  if (is_held == 1)
+    return FALSE;
+
+  gum_spinlock_acquire (spinlock);
+
+  return TRUE;
 }
 
 __declspec (naked) void
--- gum/gumspinlock.h
+++ gum/gumspinlock.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2010-2019 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2024 Francesco Tamagni <mrmacete@protonmail.ch>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
@@ -23,6 +24,7 @@ struct _GumSpinlock
 void gum_spinlock_init (GumSpinlock * spinlock);
 
 void gum_spinlock_acquire (GumSpinlock * spinlock);
+gboolean gum_spinlock_try_acquire (GumSpinlock * spinlock);
 void gum_spinlock_release (GumSpinlock * spinlock);
 
 G_END_DECLS
