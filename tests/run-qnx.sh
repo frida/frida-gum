8d0f3958e9208c69453c146200c2550a19eb8269
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-05-10 14:39:16 +0200

    tests: Move run-qnx.sh to the new build system

--- tests/run-qnx.sh
+++ tests/run-qnx.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-gum_tests=$(dirname "$0")
-
 runner_tarball=$(mktemp "/tmp/gum-tests.XXXXXX")
 
 function dispose {
@@ -11,12 +9,8 @@ trap dispose EXIT
 
 set -ex
 
-cd "$gum_tests/../../build"
-. frida_thin-env-qnx-armeabi.rc
+make
 
-cd tmp_thin-qnx-armeabi/frida-gum
-ninja
-
-cd tests
+cd build/tests
 tar -cf "$runner_tarball" gum-tests data/
 /opt/sabrelite/run.sh "$runner_tarball" /opt/frida/gum-tests
