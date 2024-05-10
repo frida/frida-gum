3dbc8a31efa32918512213d50973b74c79724d79
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-05-10 14:29:26 +0200

    tests: Remove run scripts no longer useful
    Now that `make test` can be used instead.

--- tests/run-linux-x86_64.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-arch=x86_64
-
-gum_tests=$(dirname "$0")
-cd "$gum_tests/../../build/tmp_thin-linux-$arch/frida-gum" || exit 1
-. ../../frida_thin-env-linux-x86_64.rc
-ninja || exit 1
-tests/gum-tests "$@"
--- tests/run-macos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-arch=x86_64
-
-gum_tests=$(dirname "$0")
-cd "$gum_tests/../../build/tmp-macos-$arch/frida-gum" || exit 1
-. ../../frida-env-macos-x86_64.rc
-ninja || exit 1
-tests/gum-tests "$@"
