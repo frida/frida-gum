942e753b3cf1096356ce9651aa081bb738fba5d2
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-05-10 15:32:16 +0200

    tests: Fix deprecated API usage in Stalker macOS/x86 tests

--- tests/core/arch-x86/stalker-x86-macos.m
+++ tests/core/arch-x86/stalker-x86-macos.m
@@ -93,7 +93,7 @@ create_and_destroy_window (void)
 {
   id window = [[NSWindow alloc]
       initWithContentRect:NSMakeRect (0, 0, 200, 200)
-                styleMask:NSTitledWindowMask
+                styleMask:NSWindowStyleMaskTitled
                   backing:NSBackingStoreBuffered
                     defer:NO];
   [window cascadeTopLeftFromPoint:NSMakePoint (20, 20)];
@@ -109,7 +109,7 @@ create_and_destroy_window (void)
     dispatch_async (dispatch_get_main_queue (), ^{
       [NSApp stop:NSApp];
 
-      NSEvent * event = [NSEvent otherEventWithType:NSApplicationDefined
+      NSEvent * event = [NSEvent otherEventWithType:NSEventTypeApplicationDefined
                                            location:NSMakePoint (0, 0)
                                       modifierFlags:0
                                           timestamp:0.0
