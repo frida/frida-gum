58fce3438d1d89e20014875311ffe70623a34be6
Ole André Vadla Ravnås <oleavr@gmail.com>
2024-05-10 14:26:12 +0200

    tests: Move run-android.sh to the new build system

--- tests/run-android.sh
+++ tests/run-android.sh
@@ -5,15 +5,9 @@ apex_libdirs=$2
 
 remote_prefix=/data/local/tmp/frida-tests-$arch
 
-gum_tests=$(dirname "$0")
-
 set -e
-cd "$gum_tests/../.."
-. build/frida-env-android-$arch.rc
-set -x
-cd build/tmp-android-$arch/frida-gum
-ninja
-cd tests
+make
+cd build/tests
 adb shell "mkdir $remote_prefix 2>/dev/null || true"
 adb push gum-tests data $remote_prefix
 adb shell "LD_LIBRARY_PATH='$apex_libdirs' $remote_prefix/gum-tests $@"
