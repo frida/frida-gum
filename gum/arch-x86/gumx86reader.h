fa73ee67d6f2fe16c915cb0876f3992ed47aaff0
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-02-10 22:05:20 +0100

    x86-reader: Add missing GUM_API annotations
    Co-authored-by: Håvard Sørbø <havard@hsorbo.no>

--- gum/arch-x86/gumx86reader.h
+++ gum/arch-x86/gumx86reader.h
@@ -13,13 +13,17 @@
 
 G_BEGIN_DECLS
 
-guint gum_x86_reader_insn_length (guint8 * code);
-gboolean gum_x86_reader_insn_is_jcc (const cs_insn * insn);
+GUM_API guint gum_x86_reader_insn_length (guint8 * code);
+GUM_API gboolean gum_x86_reader_insn_is_jcc (const cs_insn * insn);
 
-gpointer gum_x86_reader_try_get_relative_call_target (gconstpointer address);
-gpointer gum_x86_reader_try_get_relative_jump_target (gconstpointer address);
-gpointer gum_x86_reader_try_get_indirect_jump_target (gconstpointer address);
-cs_insn * gum_x86_reader_disassemble_instruction_at (gconstpointer address);
+GUM_API gpointer gum_x86_reader_try_get_relative_call_target (
+    gconstpointer address);
+GUM_API gpointer gum_x86_reader_try_get_relative_jump_target (
+    gconstpointer address);
+GUM_API gpointer gum_x86_reader_try_get_indirect_jump_target (
+    gconstpointer address);
+GUM_API cs_insn * gum_x86_reader_disassemble_instruction_at (
+    gconstpointer address);
 
 G_END_DECLS
 
