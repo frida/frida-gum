c1c3ebfb06e6966a4b7a8223f23a2eb83e09622c
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-02-16 19:01:32 +0100

    prof: Rename WallclockSampler to WallClockSampler
    This aligns the naming with standard capitalization conventions
    and makes the codebase more consistent.

--- libs/gum/prof/gumwallclocksampler.h
+++ libs/gum/prof/gumwallclocksampler.h
@@ -1,22 +1,22 @@
 /*
- * Copyright (C) 2009-2018 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2009-2025 Ole André Vadla Ravnås <oleavr@nowsecure.com>
  * Copyright (C) 2009 Christian Berentsen <jc.berentsen@gmail.com>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
 
-#ifndef __GUM_WALLCLOCK_SAMPLER_H__
-#define __GUM_WALLCLOCK_SAMPLER_H__
+#ifndef __GUM_WALL_CLOCK_SAMPLER_H__
+#define __GUM_WALL_CLOCK_SAMPLER_H__
 
 #include "gumsampler.h"
 
 G_BEGIN_DECLS
 
-#define GUM_TYPE_WALLCLOCK_SAMPLER (gum_wallclock_sampler_get_type ())
-G_DECLARE_FINAL_TYPE (GumWallclockSampler, gum_wallclock_sampler, GUM,
-    WALLCLOCK_SAMPLER, GObject)
+#define GUM_TYPE_WALL_CLOCK_SAMPLER (gum_wall_clock_sampler_get_type ())
+G_DECLARE_FINAL_TYPE (GumWallClockSampler, gum_wall_clock_sampler, GUM,
+    WALL_CLOCK_SAMPLER, GObject)
 
-GUM_API GumSampler * gum_wallclock_sampler_new (void);
+GUM_API GumSampler * gum_wall_clock_sampler_new (void);
 
 G_END_DECLS
 
