8b7c3e81027d3c22e7953e7fe28f04c0ac4f99e7
Ole André Vadla Ravnås <oleavr@gmail.com>
2025-07-22 14:50:06 +0200

    barebone: Support registering modules

--- gum/backend-barebone/gummoduleregistry-barebone.c
+++ gum/backend-barebone/gummoduleregistry-barebone.c
@@ -6,12 +6,27 @@
 
 #include "gummoduleregistry-priv.h"
 
+#include "gum/gumbarebone.h"
+
 void
 _gum_module_registry_activate (GumModuleRegistry * self)
 {
+  gum_barebone_on_registry_activating (self);
 }
 
 void
 _gum_module_registry_deactivate (GumModuleRegistry * self)
 {
+}
+
+G_GNUC_WEAK void
+gum_barebone_on_registry_activating (GumModuleRegistry * registry)
+{
+}
+
+void
+gum_barebone_register_module (GumModuleRegistry * registry,
+                              GumModule * module)
+{
+  _gum_module_registry_register (registry, module);
 }
--- gum/backend-barebone/include/gum/gumbarebone.h
+++ gum/backend-barebone/include/gum/gumbarebone.h
@@ -8,10 +8,15 @@
 #define __GUM_BAREBONE_H__
 
 #include <gum/gummemory.h>
+#include <gum/gummoduleregistry.h>
 
 G_BEGIN_DECLS
 
 GUM_API guint gum_barebone_query_page_size (void);
+
+GUM_API void gum_barebone_on_registry_activating (GumModuleRegistry * registry);
+GUM_API void gum_barebone_register_module (GumModuleRegistry * registry,
+    GumModule * module);
 
 G_END_DECLS
 
