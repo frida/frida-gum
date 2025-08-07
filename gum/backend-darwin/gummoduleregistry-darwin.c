5343dead2642f83e16db34a1bc7229413010265c
Francesco Tamagni <mrmacete@protonmail.ch>
2025-08-07 12:03:04 +0200

    Avoid using Interceptor

--- gum/backend-darwin/gummoduleregistry-darwin.c
+++ gum/backend-darwin/gummoduleregistry-darwin.c
@@ -31,14 +31,12 @@ struct _GumDyldNotifierContext
   DyldImageNotifier original;
 };
 
-static void gum_lldb_image_notifier (enum dyld_image_mode mode,
-    guint32 info_count, const struct dyld_image_info info[]);
 static void gum_module_registry_on_image_added (const struct mach_header * mh,
     intptr_t vmaddr_slide);
 static void gum_module_registry_on_image_removed (const struct mach_header * mh,
     intptr_t vmaddr_slide);
-static void gum_module_registry_on_dyld_notification (
-    GumInvocationContext * context, gpointer user_data);
+static void gum_lldb_image_notifier (enum dyld_image_mode mode,
+    guint32 info_count, const struct dyld_image_info info[]);
 
 static void gum_add_image (const struct mach_header * mh, const gchar * name);
 static void gum_remove_image (const struct mach_header * mh);
@@ -47,8 +45,6 @@ static gsize gum_detect_macho_size (const struct mach_header * mh);
 
 static GumModuleRegistry * gum_registry;
 static GumDarwinModuleResolver * gum_resolver;
-static GumInterceptor * gum_dyld_interceptor;
-static GumInvocationListener * gum_dyld_handler;
 static GumDyldNotifierContext * gum_dyld_notifier_context;
 
 void
@@ -66,61 +62,30 @@ _gum_module_registry_activate (GumModuleRegistry * self)
 
   if (gum_process_get_teardown_requirement () == GUM_TEARDOWN_REQUIREMENT_FULL)
   {
+    gpointer * slot;
     uint32_t count, i;
 
-    if (infos.dyld_all_image_infos_address != 0)
+    g_assert (infos.dyld_all_image_infos_address != 0);
+
+    if (infos.format == TASK_DYLD_ALL_IMAGE_INFO_64)
     {
-      gpointer * slot;
-
-      if (infos.format == TASK_DYLD_ALL_IMAGE_INFO_64)
-      {
-        slot = gum_strip_code_pointer (
-            GSIZE_TO_POINTER (infos.dyld_all_image_infos_address +
-                offsetof (DyldAllImageInfos64, notification)));
-      }
-      else
-      {
-        slot = GSIZE_TO_POINTER (infos.dyld_all_image_infos_address +
-            offsetof (DyldAllImageInfos32, notification));
-      }
-
-      gum_dyld_notifier_context = g_slice_new (GumDyldNotifierContext);
-
-      gum_dyld_notifier_context->slot = slot;
-      gum_dyld_notifier_context->original = *slot;
-
-      *slot = gum_sign_code_pointer (
-          gum_strip_code_pointer (&gum_lldb_image_notifier));
+      slot = gum_strip_code_pointer (
+          GSIZE_TO_POINTER (infos.dyld_all_image_infos_address +
+              offsetof (DyldAllImageInfos64, notification)));
     }
     else
     {
-      G_GNUC_UNUSED gconstpointer notification_impl;
-      G_GNUC_UNUSED cs_insn * first_instruction;
-      gsize offset = 0;
-
-      notification_impl = GSIZE_TO_POINTER (
-          gum_strip_code_address (infos.notification_address));
-
-#if defined (HAVE_I386)
-      first_instruction =
-          gum_x86_reader_disassemble_instruction_at (notification_impl);
-      if (first_instruction != NULL && first_instruction->id == X86_INS_INT3)
-        offset = first_instruction->size;
-#elif defined (HAVE_ARM64)
-      first_instruction =
-          gum_arm64_reader_disassemble_instruction_at (notification_impl);
-      if (first_instruction != NULL && first_instruction->id == ARM64_INS_BRK)
-        offset = first_instruction->size;
-#endif
-
-      gum_dyld_interceptor = gum_interceptor_obtain ();
-      gum_dyld_handler = gum_make_probe_listener (
-          gum_module_registry_on_dyld_notification, NULL, NULL);
-
-      gum_interceptor_attach (gum_dyld_interceptor,
-          (gpointer) (notification_impl + offset), gum_dyld_handler, NULL,
-          GUM_ATTACH_FLAGS_UNIGNORABLE);
+      slot = GSIZE_TO_POINTER (infos.dyld_all_image_infos_address +
+          offsetof (DyldAllImageInfos32, notification));
     }
+
+    gum_dyld_notifier_context = g_slice_new (GumDyldNotifierContext);
+
+    gum_dyld_notifier_context->slot = slot;
+    gum_dyld_notifier_context->original = *slot;
+
+    *slot = gum_sign_code_pointer (
+        gum_strip_code_pointer (&gum_lldb_image_notifier));
 
     do
     {
@@ -144,17 +109,6 @@ _gum_module_registry_activate (GumModuleRegistry * self)
 void
 _gum_module_registry_deactivate (GumModuleRegistry * self)
 {
-  if (gum_dyld_handler != NULL)
-  {
-    gum_interceptor_detach (gum_dyld_interceptor, gum_dyld_handler);
-
-    g_object_unref (gum_dyld_handler);
-    gum_dyld_handler = NULL;
-
-    g_object_unref (gum_dyld_interceptor);
-    gum_dyld_interceptor = NULL;
-  }
-
   if (gum_dyld_notifier_context != NULL)
   {
     GumDyldNotifierContext * context = gum_dyld_notifier_context;
@@ -179,32 +133,6 @@ gum_module_registry_on_image_removed (const struct mach_header * mh,
                                       intptr_t vmaddr_slide)
 {
   gum_remove_image (mh);
-}
-
-static void
-gum_module_registry_on_dyld_notification (GumInvocationContext * context,
-                                          gpointer user_data)
-{
-  enum dyld_image_mode mode;
-  uint32_t info_count;
-  const struct dyld_image_info * info;
-  uint32_t i;
-
-  mode =
-      GPOINTER_TO_SIZE (gum_invocation_context_get_nth_argument (context, 0));
-  if (mode != dyld_image_adding && mode != dyld_image_removing)
-    return;
-  info_count =
-      GPOINTER_TO_UINT (gum_invocation_context_get_nth_argument (context, 1));
-  info = gum_invocation_context_get_nth_argument (context, 2);
-
-  for (i = 0; i != info_count; i++)
-  {
-    if (mode == dyld_image_adding)
-      gum_add_image (info[i].imageLoadAddress, NULL);
-    else
-      gum_remove_image (info[i].imageLoadAddress);
-  }
 }
 
 static void
