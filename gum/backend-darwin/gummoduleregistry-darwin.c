435f44f15a8accd07aafc5821373bbf820d3176a
Francesco Tamagni <mrmacete@protonmail.ch>
2025-08-07 15:46:33 +0200

    darwin-module: Swap-hook LLDB image notifier (#1057)
    Since on modern iOS devices the `lldb_image_notifier` function is only
    one instruction long, Interceptor currently fails to attach it. When
    this happens, the module registry is left non-functional.
    
    This change tries first to swap the pointer in the dyld "all image
    infos" structure with our own, so there's no need to use Interceptor
    in order to get image load/unload events.

--- gum/backend-darwin/gummoduleregistry-darwin.c
+++ gum/backend-darwin/gummoduleregistry-darwin.c
@@ -7,24 +7,30 @@
 #include "gummoduleregistry-priv.h"
 
 #include "gum/gumdarwin.h"
+#include "gumdarwin-priv.h"
 #include "gummodule-darwin.h"
-#if defined (HAVE_I386)
-# include "gumx86reader.h"
-#elif defined (HAVE_ARM64)
-# include "gumarm64reader.h"
-#endif
 
 #include <dlfcn.h>
 #include <string.h>
 #include <mach-o/dyld.h>
 #include <mach-o/dyld_images.h>
 
+typedef struct _GumDyldNotifierContext GumDyldNotifierContext;
+typedef void (* DyldImageNotifier) (enum dyld_image_mode mode,
+    guint32 info_count, const struct dyld_image_info info[]);
+
+struct _GumDyldNotifierContext
+{
+  gpointer * slot;
+  DyldImageNotifier original;
+};
+
 static void gum_module_registry_on_image_added (const struct mach_header * mh,
     intptr_t vmaddr_slide);
 static void gum_module_registry_on_image_removed (const struct mach_header * mh,
     intptr_t vmaddr_slide);
-static void gum_module_registry_on_dyld_notification (
-    GumInvocationContext * context, gpointer user_data);
+static void gum_lldb_image_notifier (enum dyld_image_mode mode,
+    guint32 info_count, const struct dyld_image_info info[]);
 
 static void gum_add_image (const struct mach_header * mh, const gchar * name);
 static void gum_remove_image (const struct mach_header * mh);
@@ -33,13 +39,12 @@ static gsize gum_detect_macho_size (const struct mach_header * mh);
 
 static GumModuleRegistry * gum_registry;
 static GumDarwinModuleResolver * gum_resolver;
-static GumInterceptor * gum_dyld_interceptor;
-static GumInvocationListener * gum_dyld_handler;
+static GumDyldNotifierContext * gum_dyld_notifier_context;
 
 void
 _gum_module_registry_activate (GumModuleRegistry * self)
 {
-  GumDarwinAllImageInfos infos;
+  GumDarwinAllImageInfos infos = { 0, };
 
   gum_registry = self;
   gum_resolver = gum_darwin_module_resolver_new_with_loader (mach_task_self (),
@@ -51,33 +56,30 @@ _gum_module_registry_activate (GumModuleRegistry * self)
 
   if (gum_process_get_teardown_requirement () == GUM_TEARDOWN_REQUIREMENT_FULL)
   {
-    G_GNUC_UNUSED gconstpointer notification_impl;
-    G_GNUC_UNUSED cs_insn * first_instruction;
-    gsize offset = 0;
+    gpointer * slot;
     uint32_t count, i;
 
-    notification_impl = GSIZE_TO_POINTER (
-        gum_strip_code_address (infos.notification_address));
+    g_assert (infos.dyld_all_image_infos_address != 0);
 
-#if defined (HAVE_I386)
-    first_instruction =
-        gum_x86_reader_disassemble_instruction_at (notification_impl);
-    if (first_instruction != NULL && first_instruction->id == X86_INS_INT3)
-      offset = first_instruction->size;
-#elif defined (HAVE_ARM64)
-    first_instruction =
-        gum_arm64_reader_disassemble_instruction_at (notification_impl);
-    if (first_instruction != NULL && first_instruction->id == ARM64_INS_BRK)
-      offset = first_instruction->size;
-#endif
+    if (infos.format == TASK_DYLD_ALL_IMAGE_INFO_64)
+    {
+      slot = gum_strip_code_pointer (
+          GSIZE_TO_POINTER (infos.dyld_all_image_infos_address +
+              offsetof (DyldAllImageInfos64, notification)));
+    }
+    else
+    {
+      slot = GSIZE_TO_POINTER (infos.dyld_all_image_infos_address +
+          offsetof (DyldAllImageInfos32, notification));
+    }
 
-    gum_dyld_interceptor = gum_interceptor_obtain ();
-    gum_dyld_handler = gum_make_probe_listener (
-        gum_module_registry_on_dyld_notification, NULL, NULL);
+    gum_dyld_notifier_context = g_slice_new (GumDyldNotifierContext);
 
-    gum_interceptor_attach (gum_dyld_interceptor,
-        (gpointer) (notification_impl + offset), gum_dyld_handler, NULL,
-        GUM_ATTACH_FLAGS_UNIGNORABLE);
+    gum_dyld_notifier_context->slot = slot;
+    gum_dyld_notifier_context->original = *slot;
+
+    *slot = gum_sign_code_pointer (
+        gum_strip_code_pointer (gum_lldb_image_notifier));
 
     do
     {
@@ -101,15 +103,13 @@ _gum_module_registry_activate (GumModuleRegistry * self)
 void
 _gum_module_registry_deactivate (GumModuleRegistry * self)
 {
-  if (gum_dyld_handler != NULL)
+  if (gum_dyld_notifier_context != NULL)
   {
-    gum_interceptor_detach (gum_dyld_interceptor, gum_dyld_handler);
+    GumDyldNotifierContext * context = gum_dyld_notifier_context;
+    gum_dyld_notifier_context = NULL;
 
-    g_object_unref (gum_dyld_handler);
-    gum_dyld_handler = NULL;
-
-    g_object_unref (gum_dyld_interceptor);
-    gum_dyld_interceptor = NULL;
+    *context->slot = context->original;
+    g_slice_free (GumDyldNotifierContext, context);
   }
 
   g_clear_object (&gum_resolver);
@@ -130,21 +130,11 @@ gum_module_registry_on_image_removed (const struct mach_header * mh,
 }
 
 static void
-gum_module_registry_on_dyld_notification (GumInvocationContext * context,
-                                          gpointer user_data)
+gum_lldb_image_notifier (enum dyld_image_mode mode,
+                         guint32 info_count,
+                         const struct dyld_image_info info[])
 {
-  enum dyld_image_mode mode;
-  uint32_t info_count;
-  const struct dyld_image_info * info;
   uint32_t i;
-
-  mode =
-      GPOINTER_TO_SIZE (gum_invocation_context_get_nth_argument (context, 0));
-  if (mode != dyld_image_adding && mode != dyld_image_removing)
-    return;
-  info_count =
-      GPOINTER_TO_UINT (gum_invocation_context_get_nth_argument (context, 1));
-  info = gum_invocation_context_get_nth_argument (context, 2);
 
   for (i = 0; i != info_count; i++)
   {
@@ -153,6 +143,8 @@ gum_module_registry_on_dyld_notification (GumInvocationContext * context,
     else
       gum_remove_image (info[i].imageLoadAddress);
   }
+
+  return gum_dyld_notifier_context->original (mode, info_count, info);
 }
 
 static void
--- gum/backend-darwin/gumprocess-darwin.c
+++ gum/backend-darwin/gumprocess-darwin.c
@@ -899,6 +899,12 @@ gum_darwin_query_all_image_infos (mach_port_t task,
 
     infos->dyld_image_load_address = all_info->dyld_image_load_address;
 
+    if (all_info->version >= 9)
+    {
+      infos->dyld_all_image_infos_address =
+          all_info->dyld_all_image_infos_address;
+    }
+
     if (all_info->version >= 15)
       infos->shared_cache_base_address = all_info->shared_cache_base_address;
 
