4992d170e9f5fa0c23e2b5a371a1e7116693ff15
Ole André Vadla Ravnås <oleavr@frida.re>
2026-05-08 10:14:01 +0200

    Update copyright header

--- gum/backend-darwin/gummemory-darwin.c
+++ gum/backend-darwin/gummemory-darwin.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010-2025 Ole André Vadla Ravnås <oleavr@nowsecure.com>
- * Copyright (C) 2025 Francesco Tamagni <mrmacete@protonmail.ch>
+ * Copyright (C) 2025-2026 Francesco Tamagni <mrmacete@protonmail.ch>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
