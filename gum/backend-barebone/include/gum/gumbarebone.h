d71be21302b0de47bdaf9a822f96d5922e953a22
Ole André Vadla Ravnås <oleavr@frida.re>
2026-08-14 12:13:51 +0200

    barebone: Dispatch faults to Exceptor
    Whoever owns the target's fault vector calls
    gum_barebone_handle_exception() with an already classified
    exception, and the backend turns it into a handler call.

--- gum/backend-barebone/include/gum/gumbarebone.h
+++ gum/backend-barebone/include/gum/gumbarebone.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Ole André Vadla Ravnås <oleavr@nowsecure.com>
+ * Copyright (C) 2025-2026 Ole André Vadla Ravnås <oleavr@nowsecure.com>
  *
  * Licence: wxWindows Library Licence, Version 3.1
  */
@@ -7,6 +7,7 @@
 #ifndef __GUM_BAREBONE_H__
 #define __GUM_BAREBONE_H__
 
+#include <gum/gumexceptor.h>
 #include <gum/gummemory.h>
 #include <gum/gummoduleregistry.h>
 
@@ -15,6 +16,9 @@ G_BEGIN_DECLS
 GUM_API guint gum_barebone_query_page_size (void);
 GUM_API gpointer gum_barebone_try_remap_writable_pages (gconstpointer * addrs,
     guint n_addrs);
+
+GUM_API gboolean gum_barebone_handle_exception (GumExceptionType type,
+    gpointer pc, gpointer accessed_address, GumCpuContext * cpu_context);
 
 GUM_API void gum_barebone_on_registry_activating (GumModuleRegistry * registry);
 GUM_API void gum_barebone_register_module (GumModuleRegistry * registry,
